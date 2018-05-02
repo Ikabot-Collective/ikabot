06fdbc28d087cadcf661bfaef3d77a77f5d63b32
santipecin <santipecin@github.com>
2018-05-01 21:42:41 -0300

    agrego carpetas con init.py todavia no anda pero va tomando forma de paquete

---
+++ __init__.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import time
+import json
+import re
+import math
+from decimal import *
+import getpass
+import random
+import subprocess
+import signal
+import traceback
+import sesion
+import getJson
+import subirEdificio
+import getIds
+import getStatus
+import enter
+import run
+import clear
+import update
+import fork
+import signals
+import read
