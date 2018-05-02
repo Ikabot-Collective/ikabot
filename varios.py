9065bfcce1774e83fbbbd03ae68f199b82a6a947
physics-sp <physics-sp@protonmail.com>
2018-05-02 14:57:44 -0300

    add stuff

---
+++ varios.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def addPuntos(num):
+	return '{0:,}'.format(int(num)).replace(',','.')
+
+def diasHorasMinutos(segundosTotales):
+	dias = int(segundosTotales / Decimal(86400))
+	segundosTotales -= dias * Decimal(86400)
+	horas = int(segundosTotales / Decimal(3600))
+	segundosTotales -= horas * Decimal(3600)
+	minutos = int(segundosTotales / Decimal(60))
+	return (dias, horas, minutos)
