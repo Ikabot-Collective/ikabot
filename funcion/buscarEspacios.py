22a80b38ddcc55113cc491f16d1161e7cb5bd012
santipcn <santipcn@github.com>
2018-06-20 17:51:30 -0300

    muevo algoritmo de buscarEspacios a metodo separado tambien arreglo error de nombre, ciudadAhora cambio a ciudad

--- funcion/buscarEspacios.py
+++ funcion/buscarEspacios.py
@@ -24,45 +24,48 @@ def buscarEspacios(s):
 	info = '\nBusco espacios nuevos en las islas cada 1 hora\n'
 	setInfoSignal(s, info)
 	idIslas = getIdsdeIslas(s)
-	isla_ciudades = {}
 	try:
-		while True:
-			for idIsla in idIslas:
-				html = s.get(urlIsla + idIsla)
-				isla = getIsla(html)
-				ciudades = []
-				for ciudad in isla['cities']:
-					if ciudad['type'] != 'empty':
-						ciudades.append(ciudad)
-
-				if idIsla in isla_ciudades:
-					ciudadesAntes = isla_ciudades[idIsla]
-
-					# alguien desaparecio
-					for cityAntes in ciudadesAntes:
-						encontrado = False
-						for ciudad in ciudades:
-							if ciudad['id'] == cityAntes['id']:
-								encontrado = True
-								break
-						if encontrado is False:
-							msg = 'la ciudad {} del jugador {} desapareció en {} {}:{} {}'.format(cityAntes['name'], cityAntes['Name'], tipoDeBien[int(isla['good'])], isla['x'], isla['y'], isla['name'])
-							sendToBot(s, msg)
-
-					# alguien fundo
-					for ciudad in ciudades:
-						encontrado = False
-						for cityAntes in ciudadesAntes:
-							if ciudad['id'] == cityAntes['id']:
-								encontrado = True
-								break
-						if encontrado is False:
-							msg = '{} fundó {} en {} {}:{} {}'.format(cityAhora['Name'], cityAhora['name'], tipoDeBien[int(isla['good'])], isla['x'], isla['y'], isla['name'])
-							sendToBot(s, msg)
-
-				isla_ciudades[idIsla] = ciudades
-			time.sleep(1*60*60)
+		searchIslas(s, idIslas)
 	except:
 		msg = 'Error en:\n{}\nCausa:\n{}'.format(info, traceback.format_exc())
 		sendToBot(s, msg)
 		s.logout()
+
+def searchIslas(s, idIslas):
+	isla_ciudades = {}
+	while True:
+		for idIsla in idIslas:
+			html = s.get(urlIsla + idIsla)
+			isla = getIsla(html)
+			ciudades = []
+			for ciudad in isla['cities']:
+				if ciudad['type'] != 'empty':
+					ciudades.append(ciudad)
+
+			if idIsla in isla_ciudades:
+				ciudadesAntes = isla_ciudades[idIsla]
+
+				# alguien desaparecio
+				for cityAntes in ciudadesAntes:
+					encontrado = False
+					for ciudad in ciudades:
+						if ciudad['id'] == cityAntes['id']:
+							encontrado = True
+							break
+					if encontrado is False:
+						msg = 'la ciudad {} del jugador {} desapareció en {} {}:{} {}'.format(cityAntes['name'], cityAntes['Name'], tipoDeBien[int(isla['good'])], isla['x'], isla['y'], isla['name'])
+						sendToBot(s, msg)
+
+				# alguien fundo
+				for ciudad in ciudades:
+					encontrado = False
+					for cityAntes in ciudadesAntes:
+						if ciudad['id'] == cityAntes['id']:
+							encontrado = True
+							break
+					if encontrado is False:
+						msg = '{} fundó {} en {} {}:{} {}'.format(ciudad['Name'], ciudad['name'], tipoDeBien[int(isla['good'])], isla['x'], isla['y'], isla['name'])
+						sendToBot(s, msg)
+
+			isla_ciudades[idIsla] = ciudades
+		time.sleep(1*60*60)
