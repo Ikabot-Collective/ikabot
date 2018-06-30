aced73eedf70784f5fd2d010a5b457bead87e053
physics-sp <physics-sp@protonmail.com>
2018-06-30 16:14:09 -0300

    adapto funcion de actualizar ikabot

--- ikabot/funcion/update.py
+++ ikabot/funcion/update.py
@@ -5,8 +5,8 @@ from ikabot.helpers.process import run
 from ikabot.helpers.gui import *
 
 def update(s):
-	out = run('git pull').read().decode("utf-8") 
-	if 'Already up' in out:
+	out = run('python3 -m pip install --upgrade ikabot').read().decode("utf-8") 
+	if 'up-to-date' in out:
 		print('\nEstá actualizado')
 	else:
 		clear()
