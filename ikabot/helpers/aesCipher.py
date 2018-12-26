ef963980db9d9f695da94579e1bc24cd87430098
santipcn <santipecin@est.frba.utn.edu.ar>
2018-12-26 12:59:29 -0300

    encripcion funcionando

--- ikabot/helpers/aesCipher.py
+++ ikabot/helpers/aesCipher.py
@@ -19,7 +19,7 @@ class AESCipher:
         raw = pad(raw)
         iv = Random.new().read( AES.block_size )
         cipher = AES.new( self.key, AES.MODE_CBC, iv )
-        return base64.b64encode( iv + cipher.encrypt( raw ) )
+        return base64.b64encode( iv + cipher.encrypt( raw ) ).decode('utf-8')
 
     def decrypt( self, enc ):
         enc = base64.b64decode(enc)
