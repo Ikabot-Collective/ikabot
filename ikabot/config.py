dd9540588c418664e1d3180d21c9022bc27c3c52
santipcn <santiago.pecin@protonmail.com>
2019-02-12 04:56:16 -0300

    agrego sendToBotDebug se activa en el archivo de config

--- ikabot/config.py
+++ ikabot/config.py
@@ -9,3 +9,12 @@ urlCiudad = 'view=city&cityId='
 urlIsla = 'view=island&islandId='
 prompt = ' >>  '
 tipoDeBien = ['Madera', 'Vino', 'Marmol', 'Cristal', 'Azufre']
+debugON_alertarAtaques    = False
+debugON_alertarPocoVino   = False
+debugON_botDonador        = False
+debugON_buscarEspacios    = False
+debugON_entrarDiariamente = False
+debugON_enviarVino        = False
+debugON_menuRutaComercial = False
+debugON_subirEdificio     = False
+debugON_session           = False
