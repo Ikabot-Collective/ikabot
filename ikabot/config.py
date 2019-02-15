65446efa267fdbccb932501402811af55edf0f56
physics-sp <physics-sp@protonmail.com>
2019-02-15 11:42:33 -0300

    muevo archivos cook y telegram a home directory del usr

--- ikabot/config.py
+++ ikabot/config.py
@@ -3,8 +3,8 @@ import getpass
 ids = None
 ciudades = None
 infoUser = ''
-cookieFile = '/tmp/.cookies_of_{}.txt'.format(getpass.getuser())
-telegramFile = '/tmp/.telegram_of_{}.txt'.format(getpass.getuser())
+cookieFile = '~/.cookies_of_{}.txt'.format(getpass.getuser())
+telegramFile = '~/.telegram_of_{}.txt'.format(getpass.getuser())
 urlCiudad = 'view=city&cityId='
 urlIsla = 'view=island&islandId='
 prompt = ' >>  '
