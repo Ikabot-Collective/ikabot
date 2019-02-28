e2d677adadb4c7f6f1941029596250f420184b17
physics-sp <physics-sp@protonmail.com>
2019-02-28 14:39:58 -0300

    aumento version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="ikabot",
-	version="3.0",
+	version="3.2",
 	author="physics-sp",
 	description="A bot for ikariam",
 	long_description=long_description,
