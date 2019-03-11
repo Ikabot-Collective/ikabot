e817185e8d4a3d976dae6eaab8cec9c8934a298c
santipcn <santiago.pecin@protonmail.com>
2019-03-10 21:35:24 -0300

    bug fix en buscarEspacios ya no detecta tres veces que alguien fundo/desaparecio

--- ikabot/funcion/buscarEspacios.py
+++ ikabot/funcion/buscarEspacios.py
@@ -6,6 +6,7 @@ import traceback
 from ikabot.config import *
 from ikabot.helpers.botComm import *
 from ikabot.helpers.gui import enter
+from ikabot.helpers.varios import esperar
 from ikabot.helpers.signals import setInfoSignal
 from ikabot.helpers.pedirInfo import getIdsdeIslas
 from ikabot.helpers.getJson import getIsla
@@ -23,22 +24,21 @@ def buscarEspacios(s):
 
 	info = '\nBusco espacios nuevos en las islas cada 1 hora\n'
 	setInfoSignal(s, info)
-	idIslas = getIdsdeIslas(s)
 	try:
-		do_it(s, idIslas)
+		do_it(s)
 	except:
 		msg = 'Error en:\n{}\nCausa:\n{}'.format(info, traceback.format_exc())
 		sendToBot(msg)
 	finally:
 		s.logout()
 
-def do_it(s, idIslas):
+def do_it(s):
 	isla_ciudades = {}
 	while True:
+		idIslas = getIdsdeIslas(s)
 		for idIsla in idIslas:
 			html = s.get(urlIsla + idIsla)
 			isla = getIsla(html)
-			ciudades = []
 			ciudades = [ciudad for ciudad in isla['cities'] if ciudad['type'] != 'empty']
 
 			if idIsla in isla_ciudades:
@@ -62,5 +62,5 @@ def do_it(s, idIslas):
 						msg = '{} fundó {} en {} {}:{} {}'.format(ciudad['Name'], ciudad['name'], tipoDeBien[int(isla['good'])], isla['x'], isla['y'], isla['name'])
 						sendToBot(msg)
 
-			isla_ciudades[idIsla] = ciudades
-		time.sleep(1*60*60)
+			isla_ciudades[idIsla] = ciudades.copy()
+		esperar(1*60*60)
