a3a95e7d75a98daa02fc59ae92c9c78462ae1a13
santipcn <santiago.pecin@protonmail.com>
2019-04-09 22:03:02 -0300

    aumento la versino

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="ikabot",
-	version="4.0.1",
+	version="4.0.2",
 	author="physics-sp",
 	description="A bot for ikariam",
 	long_description=long_description,
