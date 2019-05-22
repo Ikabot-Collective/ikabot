75bd2f39ad41f5880d41af69bc3cbb5a2344f819
santipcn <santipecin@est.frba.utn.edu.ar>
2019-05-22 19:49:03 -0300

    update solo da instrucciones de como actualizar ikabot ya no lo actualiza automaticamente debido a que puede no tener los privilegios necesarios

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="ikabot",
-	version="4.3.5",
+	version="4.3.6",
 	author="physics-sp",
 	description="A bot for ikariam",
 	long_description=long_description,
