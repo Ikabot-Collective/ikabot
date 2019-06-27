08f88031a85efe08ae4b315b9089288b609de768
physics-sp <physics-sp@protonmail.com>
2019-06-26 21:20:53 -0300

    subirEdificio obtiene los recursos faltantes automagicamente

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="ikabot",
-	version="4.7.0",
+	version="4.8.0",
 	author="physics-sp",
 	description="A bot for ikariam",
 	long_description=long_description,
