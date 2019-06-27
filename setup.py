5bfded18c1d804367a51a3a91e7e47f2469efdf2
santipcn <santiago.pecin@protonmail.com>
2019-06-26 22:21:22 -0300

    aumento version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="ikabot",
-	version="4.8.1",
+	version="4.8.2",
 	author="physics-sp",
 	description="A bot for ikariam",
 	long_description=long_description,
