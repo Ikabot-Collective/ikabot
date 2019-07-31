f25787429a5ea97b4cab9db84fc8757fa7494a21
santipcn <santiago.pecin@protonmail.com>
2019-07-30 22:03:34 -0300

    subo version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
 	name="ikabot",
-	version="5.2.4",
+	version="5.2.5",
 	author="physics-sp",
 	description="A bot for ikariam",
 	url="https://github.com/physics-sp/ikabot",
