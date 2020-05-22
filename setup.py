2ab0e0a53cff9d2fd296ff0b62ff1a966a040ea5
physics-sp <physics-sp@protonmail.com>
2020-05-22 01:42:02 -0300

    improve distribution decision in repartirRecurso

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
 	name="ikabot",
-	version="5.7.16",
+	version="5.7.17",
 	author="physics-sp",
 	description="A bot for ikariam",
 	url="https://github.com/physics-sp/ikabot",
