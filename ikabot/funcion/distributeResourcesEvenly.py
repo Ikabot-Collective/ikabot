17f09645ee23e09ea81cc72fe0be53191310edac
physics-sp <physics-sp@protonmail.com>
2020-05-27 11:20:40 -0300

    add some small changes to distributeResourcesEvenly mostly translation stuff

--- ikabot/funcion/distributeResourcesEvenly.py
+++ ikabot/funcion/distributeResourcesEvenly.py
@@ -5,14 +5,14 @@ import gettext
 import traceback
 from ikabot.config import *
 from ikabot.helpers.botComm import *
-from ikabot.helpers.pedirInfo import *
-from ikabot.helpers.signals import setInfoSignal
-from ikabot.helpers.getJson import getCiudad
-from ikabot.helpers.planearViajes import planearViajes
-from ikabot.helpers.recursos import *
-from ikabot.helpers.varios import addPuntos
-from ikabot.helpers.process import forkear
 from ikabot.helpers.gui import banner
+from ikabot.helpers.recursos import *
+from ikabot.helpers.pedirInfo import *
+from ikabot.helpers.process import forkear
+from ikabot.helpers.varios import addPuntos
+from ikabot.helpers.getJson import getCiudad
+from ikabot.helpers.signals import setInfoSignal
+from ikabot.helpers.planearViajes import planearViajes
 
 t = gettext.translation('distributeResourcesEvenly',
                         localedir,
@@ -24,13 +24,14 @@ def distributeResourcesEvenly(s):
 
 	banner() #displays the ascii art banner
 
-	print(_('What resource do you want to distribute?'))
-	print(_('(0) Wood'))
-	print(_('(1) Wine'))
-	print(_('(2) Marble'))
-	print(_('(3) Crystal'))
-	print(_('(4) Sulphur'))
-	resource = read(min=0, max=4)
+	print(_('¿Qué recurso quiere distribuir?'))
+	print(_('(0) Salir'))
+	for i in range(4):
+		print('({:d}) {}'.format(i+1, tipoDeBien[i]))
+	resource = read(min=0, max=5)
+	if resource == 0:
+		return
+	resource -= 1
 
 	resourceTotal = 0 #total number of selected resource
 	(cityIDs, cities) = getIdsDeCiudades(s) #gets city ids
@@ -66,61 +67,56 @@ def distributeResourcesEvenly(s):
 	routes = []
 	
 	for originCityID in originCities: #iterate through all origin city ids
+		if originCities[originCityID] == 0:
+			continue
 		for destinationCityID in destinationCities: #iterate through all destination city ids
-			
-			if originCities[originCityID] > destinationCities[destinationCityID] and destinationCities[destinationCityID] != 0 and originCities[originCityID] != 0: #if there's more resources above average in the origin city than resources below average in the destination city (origin city needs to have a surplus and destination city needs to have a deficit of resources for a route to be considered)
-				toSend =destinationCities[destinationCityID] #number of resources to send is the number of resources below average in destination city
-				# ROUTE BLOCK
-				if resource == 1:
-					route=(allCities[originCityID],allCities[destinationCityID],allCities[destinationCityID]['islandId'],0,toSend,0,0,0)
-				elif resource == 2:
-					route=(allCities[originCityID],allCities[destinationCityID],allCities[destinationCityID]['islandId'],0,0,toSend,0,0)
-				elif resource == 3:
-					route=(allCities[originCityID],allCities[destinationCityID],allCities[destinationCityID]['islandId'],0,0,0,toSend,0)
-				elif resource == 4:
-					route=(allCities[originCityID],allCities[destinationCityID],allCities[destinationCityID]['islandId'],0,0,0,0,toSend)
-				else:
-					route=(allCities[originCityID],allCities[destinationCityID],allCities[destinationCityID]['islandId'],toSend,0,0,0,0)
-				routes.append(route)
-				# ROUTE BLOCK
+			if destinationCities[destinationCityID] == 0:
+				continue
+
+			if originCities[originCityID] > destinationCities[destinationCityID]: #if there's more resources above average in the origin city than resources below average in the destination city (origin city needs to have a surplus and destination city needs to have a deficit of resources for a route to be considered)
+				toSend = destinationCities[destinationCityID] #number of resources to send is the number of resources below average in destination city
+			else:
+				toSend = originCities[originCityID] #send the amount of resources above average of the current origin city
+
+			# ROUTE BLOCK
+			if resource == 0:
+				route = (allCities[originCityID],allCities[destinationCityID],allCities[destinationCityID]['islandId'],toSend,0,0,0,0)
+			elif resource == 1:
+				route = (allCities[originCityID],allCities[destinationCityID],allCities[destinationCityID]['islandId'],0,toSend,0,0,0)
+			elif resource == 2:
+				route = (allCities[originCityID],allCities[destinationCityID],allCities[destinationCityID]['islandId'],0,0,toSend,0,0)
+			elif resource == 3:
+				route = (allCities[originCityID],allCities[destinationCityID],allCities[destinationCityID]['islandId'],0,0,0,toSend,0)
+			elif resource == 4:
+				route = (allCities[originCityID],allCities[destinationCityID],allCities[destinationCityID]['islandId'],0,0,0,0,toSend)
+			routes.append(route)
+
+			# ROUTE BLOCK
+			if originCities[originCityID] > destinationCities[destinationCityID]:
 				originCities[originCityID] -= destinationCities[destinationCityID] #remove the sent amount from the origin city's surplus
 				destinationCities[destinationCityID] = 0 #set the amount of resources below average in destination city to 0
-
-			elif destinationCities[destinationCityID] != 0 and originCities[originCityID] != 0 :  #if there arent enough resources in the current origin city to send (origin city needs to have a surplus and destination city needs to have a deficit of resources for a route to be considered)
-				toSend = originCities[originCityID] #send the amount of resources above average of the current origin city
-				# ROUTE BLOCK
-				if resource == 1:
-					route=(allCities[originCityID],allCities[destinationCityID],allCities[destinationCityID]['islandId'],0,toSend,0,0,0)
-				elif resource == 2:
-					route=(allCities[originCityID],allCities[destinationCityID],allCities[destinationCityID]['islandId'],0,0,toSend,0,0)
-				elif resource == 3:
-					route=(allCities[originCityID],allCities[destinationCityID],allCities[destinationCityID]['islandId'],0,0,0,toSend,0)
-				elif resource == 4:
-					route=(allCities[originCityID],allCities[destinationCityID],allCities[destinationCityID]['islandId'],0,0,0,0,toSend)
-				else:
-					route=(allCities[originCityID],allCities[destinationCityID],allCities[destinationCityID]['islandId'],toSend,0,0,0,0)
-				routes.append(route)
-				# ROUTE BLOCK
+			else:
 				destinationCities[destinationCityID] -= originCities[originCityID] #remove the sent amount from the amount of resources below average in current destination city
 				originCities[originCityID] = 0 #set the amount of resources above average in origin city to 0
-	
+
 	banner()
-	print(_('\nThe following routes will be executed:'))
+	print(_('\nSe realizarán los siguientes envios:\n'))
 	for route in routes:
-		print('\n{} -> {} : {} {}'.format(route[0]['name'], route[1]['name'], route[resource+3], tipoDeBien[resource])) #displays all routes to be executed in console
+		print('{} -> {} : {} {}'.format(route[0]['name'], route[1]['name'], route[resource+3], tipoDeBien[resource])) #displays all routes to be executed in console
 
-	print('\nAll cities will have about {} {}'.format(resourceAverage, tipoDeBien[resource]))
+	print(_('\nTodas las ciuaddes tendrán al rededor de {} de {}').format(addPuntos(resourceAverage), tipoDeBien[resource]))
 
-	print(_('\nProceed? [Y/n]'))
+	print(_('\n¿Proceder? [Y/n]'))
 	rta = read(values=['y', 'Y', 'n', 'N', ''])
 	if rta.lower() == 'n':
 		return
 
-	forkear(s) #forks the process. This is needed for multithreaded operation
+	forkear(s) #forks the process.
 	if s.padre is True:
 		return
 
-	info = _('\nDistribute Resources Evenly\n')
+	info = _('\nDistribuyo {} de forma uniforme entre todas las ciudades\n').format(tipoDeBien[resource])
+	setInfoSignal(s, info)
 
 	try:
 		planearViajes(s, routes) #plan trips for all the routes
@@ -128,6 +124,4 @@ def distributeResourcesEvenly(s):
 		msg = _('Error en:\n{}\nCausa:\n{}').format(info, traceback.format_exc())
 		sendToBot(s, msg) #sends message to telegram bot
 	finally:
-		s.logout() #logs out?
-
-
+		s.logout()
