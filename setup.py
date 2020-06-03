b064e3316a3cb2ab18eed5031b24359be18d9125
physics-sp <physics-sp@protonmail.com>
2020-06-03 14:44:41 -0300

    update login

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
 	name="ikabot",
-	version="5.8.6",
+	version="5.8.7",
 	author="physics-sp",
 	description="A bot for ikariam",
 	url="https://github.com/physics-sp/ikabot",
