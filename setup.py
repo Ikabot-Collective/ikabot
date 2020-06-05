9a2d7e9a08ea0e7290b1d5b6471f5bb325b4d23e
physics-sp <physics-sp@protonmail.com>
2020-06-05 12:34:43 -0300

    add some more info to setup.py

--- setup.py
+++ setup.py
@@ -4,23 +4,24 @@ setuptools.setup(
 	name="ikabot",
 	version="6.0.4",
 	author="physics-sp",
+	author_email="physics-sp@protonmail.com",
+	license='MIT',
 	description="A bot for ikariam",
 	url="https://github.com/physics-sp/ikabot",
 	include_package_data=True,
 	packages=setuptools.find_packages(),
 	install_requires=[
 		  'requests',
+		  'requests[socks]',
 		  'cryptography',
-		  'psutil',
-		  'requests[socks]'
+		  'psutil'
 	],
 	entry_points = {
 		'console_scripts': ['ikabot=ikabot.command_line:main'],
 	},
 	classifiers=[
 		"Programming Language :: Python :: 3",
-		"License :: OSI Approved :: MIT License",
-		"Operating System :: POSIX :: Linux",
+		"License :: OSI Approved :: MIT License"
         ],
 )
 
