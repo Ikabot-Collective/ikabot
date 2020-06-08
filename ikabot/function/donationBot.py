4b796370ebff5d5d8696189d575bc268c56d58f7
physics-sp <physics-sp@protonmail.com>
2020-06-08 19:52:42 -0300

    rewrite donationBot, now it allows the user to donate a percentaje also, now it is in english

--- ikabot/function/donationBot.py
+++ ikabot/function/donationBot.py
@@ -8,6 +8,7 @@ from ikabot.config import *
 from ikabot.helpers.botComm import *
 from ikabot.helpers.pedirInfo import *
 from ikabot.helpers.gui import *
+from ikabot.helpers.varios import wait
 from ikabot.helpers.process import set_child_mode
 from ikabot.helpers.signals import setInfoSignal
 from ikabot.helpers.getJson import getCiudad
@@ -23,21 +24,35 @@ def donationBot(s,e,fd):
 	sys.stdin = os.fdopen(fd)
 	try:
 		banner()
-		(idsCiudades, ciudades) = getIdsOfCities(s)
-		ciudades_dict = {}
-		bienes = {'1': _('(W)'), '2': _('(M)'), '3': _('(C)'), '4': _('(S)')}
-		for idCiudad in idsCiudades:
-			tradegood = ciudades[idCiudad]['tradegood']
-			bien = bienes[tradegood]
-			print(_('In the city {} {}, Do you wish to donate to the forest, to the trading good or neither? [f/t/n]').format(ciudades[idCiudad]['name'], bien))
-			rta = read(values=[_('f'), _('F'), _('t'), _('T'), _('n'), _('N')])
-			if rta.lower() == _('a'):
-				tipo = 'resource'
-			elif rta.lower() == _('b'):
-				tipo = 'tradegood'
+		(cities_ids, cities) = getIdsOfCities(s)
+		cities_dict = {}
+		initials = [ material_name[0] for material_name in materials_names ]
+		for cityId in cities_ids:
+			tradegood = cities[cityId]['tradegood']
+			initial = initials[int(tradegood)]
+			print(_('In {} ({}), Do you wish to donate to the forest, to the trading good or neither? [f/t/n]').format(cities[cityId]['name'], initial))
+			f = _('f')
+			t = _('t')
+			n = _('n')
+
+			rta = read(values=[f, f.upper(), t, t.upper(), n, n.upper()])
+			if rta.lower() == f:
+				donation_type = 'resource'
+			elif rta.lower() == t:
+				donation_type = 'tradegood'
 			else:
-				tipo = None
-			ciudades_dict[idCiudad] = {'tipo': tipo}
+				donation_type = None
+				percentage = None
+
+			if donation_type is not None:
+				print(_('What percentage of the resources do you want to donate every day? (default: 100%)'))
+				percentage = read(min=0, max=100, empty=True)
+				if percentage == '':
+					percentage = 100
+				elif percentage == 0:
+					donation_type = None
+
+			cities_dict[cityId] = {'donation_type': donation_type, 'percentage': percentage}
 
 		print(_('I will donate every day.'))
 		enter()
@@ -51,26 +66,35 @@ def donationBot(s,e,fd):
 	info = _('\nI donate every day\n')
 	setInfoSignal(s, info)
 	try:
-		do_it(s, idsCiudades, ciudades_dict)
+		do_it(s, cities_ids, cities_dict)
 	except:
 		msg = _('Error in:\n{}\nCause:\n{}').format(info, traceback.format_exc())
 		sendToBot(s, msg)
 	finally:
 		s.logout()
 
-def do_it(s, idsCiudades, ciudades_dict):
-	for idCiudad in idsCiudades:
-		html = s.get(urlCiudad + idCiudad)
-		ciudad = getCiudad(html)
-		ciudades_dict[idCiudad]['isla'] = ciudad['islandId']
+def do_it(s, cities_ids, cities_dict):
+	for cityId in cities_ids:
+		html = s.get(urlCiudad + cityId)
+		city = getCiudad(html)
+		cities_dict[cityId]['island'] = city['islandId']
+
 	while True:
-		for idCiudad in idsCiudades:
-			html = s.get(urlCiudad + idCiudad)
-			madera = getRecursosDisponibles(html)[0]
-			idIsla = ciudades_dict[idCiudad]['isla']
-			tipo = ciudades_dict[idCiudad]['tipo']
-			if tipo:
-				s.post(payloadPost={'islandId': idIsla, 'type': tipo, 'action': 'IslandScreen', 'function': 'donate', 'donation': madera, 'backgroundView': 'island', 'templateView': tipo, 'actionRequest': 'REQUESTID', 'ajax': '1'})
+		for cityId in cities_ids:
+			donation_type = cities_dict[cityId]['donation_type']
+			if donation_type is None:
+				continue
+
+			html = s.get(urlCiudad + cityId)
+			wood  = getRecursosDisponibles(html, num=True)[0]
+			wood *= ( cities_dict[cityId]['percentage'] / 100 )
+			wood = int(wood)
+			islandId = cities_dict[cityId]['island']
+
+			s.post(payloadPost={'islandId': islandId, 'type': donation_type, 'action': 'IslandScreen', 'function': 'donate', 'donation': wood, 'backgroundView': 'island', 'templateView': donation_type, 'actionRequest': 'REQUESTID', 'ajax': '1'})
+
 		msg = _('I donated automatically.')
 		sendToBotDebug(s, msg, debugON_donationBot)
-		time.sleep(24*60*60)
+
+		# sleep a day
+		wait(24*60*60, maxrandom=60*60)
