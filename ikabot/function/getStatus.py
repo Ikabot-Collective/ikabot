a52b95b30a413ef9df3fd95f61877c992569095e
ikagod <jenniferwilson355.10@gmail.com>
2021-01-18 13:06:11 +0100

    getStatus.py now shows global resource status

--- ikabot/function/getStatus.py
+++ ikabot/function/getStatus.py
@@ -12,6 +12,7 @@ from ikabot.helpers.varios import *
 from ikabot.helpers.resources import *
 from ikabot.helpers.pedirInfo import *
 from ikabot.helpers.getJson import getCity
+from ikabot.helpers.market import getGold
 
 t = gettext.translation('getStatus',
                         localedir,
@@ -34,7 +35,51 @@ def getStatus(session, event, stdin_fd):
 		banner()
 		color_arr = [bcolors.ENDC, bcolors.HEADER, bcolors.STONE, bcolors.BLUE, bcolors.WARNING]
 
-		print(_('Ships {:d}/{:d}').format(getAvailableShips(session), getTotalShips(session)))
+		(ids, __) = getIdsOfCities(session)
+		total_resources = [0] * len(materials_names)
+		total_production = [0] * len(materials_names)
+		total_wine_consumption = 0
+		available_ships = 0
+		total_ships = 0
+		for id in ids:
+			session.get('view=city&cityId={}'.format(id), noIndex = True)
+			data = session.get("view=updateGlobalData&ajax=1", noIndex = True)
+			json_data = json.loads(data, strict=False)
+			json_data = json_data[0][1]['headerData']
+			if json_data['relatedCity']['owncity'] != 1:
+				continue
+			wood = Decimal( json_data['resourceProduction'] )
+			good = Decimal( json_data['tradegoodProduction'] )
+			typeGood = int( json_data['producedTradegood'] )
+			total_production[0] += wood * 3600
+			total_production[typeGood] += good * 3600
+			total_wine_consumption += json_data['wineSpendings']
+			total_resources[0] += json_data['currentResources']['resource']
+			total_resources[1] += json_data['currentResources']['1']
+			total_resources[2] += json_data['currentResources']['2']
+			total_resources[3] += json_data['currentResources']['3']
+			total_resources[4] += json_data['currentResources']['4']
+			available_ships = json_data['freeTransporters']
+			total_ships = json_data['maxTransporters']
+			total_gold = int(Decimal(json_data['gold']))
+			total_gold_production = int(Decimal(json_data['scientistsUpkeep'] + json_data['income'] + json_data['upkeep']))
+		print(_('Ships {:d}/{:d}').format(int(available_ships), int(total_ships)))
+		print(_("\nTotal:"))
+		print('{:>10}'.format(' '), end='|')
+		for i in range(len(materials_names)):
+			print('{:>12}'.format(materials_names_english[i]), end = '|')
+		print()
+		print('{:>10}'.format('Available'), end = '|')
+		for i in range(len(materials_names)):
+			print('{:>12}'.format(addThousandSeparator(total_resources[i], ' ')), end = '|')
+		print()
+		print('{:>10}'.format('Production'), end = '|')
+		for i in range(len(materials_names)):
+			print('{:>12}'.format(addThousandSeparator(total_production[i], ' ')), end = '|')
+		print()
+		print("Gold : {}, Gold production : {}".format(addThousandSeparator(total_gold, ' '), addThousandSeparator(total_gold_production, ' ')))
+		print("Wine consumption : {}".format(addThousandSeparator(total_wine_consumption, ' ')), end='')
+
 
 		print(_('\nOf which city do you want to see the state?'))
 		city = chooseCity(session)
@@ -52,14 +97,14 @@ def getStatus(session, event, stdin_fd):
 			else:
 				color_resources.append(bcolors.ENDC)
 		print(_('Storage:'))
-		print(addDot(storageCapacity))
+		print(addThousandSeparator(storageCapacity))
 		print(_('Resources:'))
 		for i in range(len(materials_names)):
-			print('{} {}{}{} '.format(materials_names[i], color_resources[i], addDot(resources[i]), bcolors.ENDC), end='')
+			print('{} {}{}{} '.format(materials_names[i], color_resources[i], addThousandSeparator(resources[i]), bcolors.ENDC), end='')
 		print('')
 
 		print(_('Production:'))
-		print('{}:{} {}:{}'.format(materials_names[0], addDot(wood*3600), materials_names[typeGood], addDot(good*3600)))
+		print('{}:{} {}:{}'.format(materials_names[0], addThousandSeparator(wood*3600), materials_names[typeGood], addThousandSeparator(good*3600)))
 
 		hasTavern = 'tavern' in [ building['building'] for building in city['position'] ]
 		if hasTavern:
