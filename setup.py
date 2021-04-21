105a11fb2c33ea324caebb40f78292aa5b2ad1fc
physics-sp <physics-sp@protonmail.com>
2021-04-21 18:05:16 -0300

    increase version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="ikabot",
-    version="6.5.14",
+    version="6.5.15",
     author="physics-sp",
     author_email="physics-sp@protonmail.com",
     license='MIT',
