543644bd29544d5836df4531dbd4ce3325a24a93
susman <61547860+johnvittu@users.noreply.github.com>
2023-05-09 22:49:36 +0300

    Clipboard, cookie.txt, disable enter
    Add copy-to-clipboard when exporting a cookie, saving cookie into cookie.txt in the script root and disable enter

--- ikabot/function/importExportCookie.py
+++ ikabot/function/importExportCookie.py
@@ -4,6 +4,8 @@ import gettext
 import json
 import sys
 import requests
+import pyperclip as pc
+import os
 from ikabot.helpers.pedirInfo import read
 from ikabot.helpers.gui import *
 from ikabot.config import *
@@ -86,4 +88,18 @@ def exportCookie(session):
     4. Press F5
     """)
     print(cookies_js)
-    enter()
+    if os.path.isfile("cookie.txt"):
+        os.remove("cookie.txt")
+        f = open("cookie.txt", "a")
+        print(cookies_js, file=f)
+        f.close()
+        pc.copy(cookies_js)
+        print('\n\nCookie has been copied to the system clipboard and cookie.txt!')
+        #enter()
+    else:
+        f = open("cookie.txt", "a")
+        print(cookies_js, file=f)
+        f.close()
+        pc.copy(cookies_js)
+        print('\n\nCookie has been copied to the system clipboard and cookie.txt!')
+        #enter()
