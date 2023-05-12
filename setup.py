697636690c757c71e56717cd43112864811d34f3
Xaalek <54487782+Xaalek@users.noreply.github.com>
2023-05-12 18:39:21 +0200

    Revert "Bump Ikabot version 6.0.1"
    This reverts commit 4458019b6adf8d313bd7de2732279d4a6c84078f.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="ikabot",
-    version="6.0.1",
+    version="6.5.35",
     author="physics-sp",
     author_email="physics-sp@protonmail.com",
     license='MIT',
