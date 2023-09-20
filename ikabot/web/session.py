858c7f9b3a9df10544bc50f473957340182899f5
ikagod <65701886+ikagod@users.noreply.github.com>
2023-09-20 21:11:06 +0200

    Update blackbox tokens (they expired?)

--- ikabot/web/session.py
+++ ikabot/web/session.py
@@ -28,17 +28,17 @@ t = gettext.translation('session', localedir, languages=languages, fallback=True
 _ = t.gettext
 
 #blackbox tokens
-blackbox_tokens = [
-    'JVqc1PkrbpPF9zyxI5ICZ4y-BFe4Kov1WtA_ZJbI7R9ih7nrYM5BsRZ54kixFnqf0QMoWp3C9CZt0jWgDzRmmL3vMleJuxJ76U28M6bL_S9UhsnuIFKYAXPYPq0lSnyu0wVIbZ_R9ihaf7H0JEl7vvIXSYyx4xV66BVqveIUV7wqT4Gz2ApNcqTWPHCn2j5yqdwMPXDUCUGkCW6l3RRHed4XToLmSn7fRXfcFHio2Qk-cqbfQnWt4RdMhOdJr-AVRXrfE0OoDHGm1vstX4S2-R5Qgsk4pw563wQ2Zq8dgK7TBTVdnusvWH2v8jOByBRZfrDgCEmW2v8xdJnL-02uEnfmVHmr2y1mi73tH09_pNYGWb4wmf5xlsj4PKUXfN9Thsr7LFGDsymc-zCPv-QWRrYpiL0cTHWazP4jVZi97yFUiO4ihb0fT4S4HFGE6Eyu4hV3qNo_ogU-btI3bZ7SBWbIATdupgs7bqfYCTudAWPFKlu_IYbqS3uz6h6BtOkfRHaozf9CZ5nL-13D-1yMwSJZvB1VhrnyKYvsIVOK602v4UJ5ruRJea_lGk6y6RpLg7obf7LmFnms4xh94hh930GkCECj2xRGdpvN_yRWmcoDNWWKvP8wYJTE6RtekMTpG17SRLkeQ3W4LJ4TeJ3PEjdpm8wyYpX3LmXLL5DD9i1hk8X7LI7zVIToGlG27CBShedNf7EVR6kPRHvhRqgLQHPUOXClB0B2qA5Eqd4QQ6YJOWuQwvQZS46z5Rd6sedMfrATdqwNbqcNQqPZDkd_uOkiWo3vJl3AJFSMwSJVi77yVrrwVozF-DBomtEylMkqXsH3WpDBIliKwidcgbPlCjx_pNYIbqHZO50CZZrL_C2S9ixcjO1OhrbvUYW27SZewSJXju8niOwjVbnxJ1eJuexNhOggWLnvH1iM8CWJviBVuhuBtdoMPmOV2P0vYZnPMZb7XML0Wo2-91yPvyFYvCKH606EuRx_4RpKfN0WTofoGkt74Uep3EB0qt4WebHhQ6fXEEJz1DVlxvgxZJa77R9EdrnsIU-GufEkVo_E_TBgmcv9IlSXvO4gg7UWTrPoSX2tD0By1jdr0QJknP8zmfowYZfNMmmiAztvpQptptY3m9EBNGXG-i2OxCdgkMctY5TK_S5ewSaKvuMVR2ye4RJLe63lGEiAsOcMPoGm2Ao8bJ7R_i5hjsD0SHmr0ANEdq7TBkd3p9UIO3TO8yVXfK7xFkh640y98GOazC-hEkW_MJX-Ysw-d-IYjweB9WibwPIkSXu-8SdXfK7xFkh6q9sAMmSJu_4jVYfeV6jgQ70KdL8Sersel9kyhvRNxCVr0yCG3BV80Emj2i9fmAFShPxVuv43kPFboBdqsvhAkNQplOAzhP1itQtVpPhFfNUcZd4rmfxougBw0ylx1PkrccDzWozYRY76Q5bYHW_HOqv4T7Mjhcn-Zcsfh-BEsOU4gvxKnOVTvjOYyDNqzTiF90eKy0OR-16T4UukG2quEUaTDXTtEkR2m80QNWeZ5lXPOKQQcZbIDkNxocb4KFCnEH7iUcg7YJLCEGSJu-scTHqqzwJEaZvLIov5L2OIu_0iVIT8MmaLvgAlV4f5b5THCDlpotAAKU6AsPdcvyqZvvA2aJjJ-SlairvgEkKI8WPILp0VOmyy4xRFc6PI-ixRg8brHU-BseMWQ3Om0wU5jb7wFUiJu_MYS4y87BpKeqoEKVuNsuQnlQp24gc8gA',
-    'JVqc1PkrbpPF9zyxI5ICZ4y-BFe4Kov1WtA_ZJbI7R9ih7nrHEFzpcr8P2SWyA9010Kx1gg6X5HU-StdtB2L717VSG2f0fYoa5DC9DqjFXrgT8fsHlB1p-oPQXOYyvwhU5bG6x1glLnrLlOFtxyKtwxfhLb5XszxI1V6rO8URnjeEkl84BRLfq7fEnar40arEEd_tukbgLnwJIjsIIHnGX62Gkp7q-AUSIHkF0-Due4mietRgrfnHIG15UquE0h4nc8BJlibwPIka9pJsByBptgIUb8iUHWn1_9AjdH6H1GU1SNqtvsgUoKq6zh8odMWO22d71C0GYj2G019zwgtX4_B8SFGeKj7YNI7oBM4apreR7kegfUobJ3O8yVVyz6d0jFhhrjoWMsqX77uFzxuoMX3Ol-Rw_YqkMQnX8HxJlq-8yaK7lCEtxlKfOFEp-AQdNkPQHSnCGqj2RBIrd0QSXqr3T-jBWfM_WHDKIztHVWMwCNWi8HmGEpvoeQJO22d_2Wd_i5jxPtev_coW5TLLY7D9SyN71GD5BtQhusbUYe88FSLvO0lXL0hVIi4G06Fuh-Euh-B40aq4kV9tugYPW-hxvg7bKXXByxeodICNmaLvQAyZou9AHTmW8DlF1rOQLUaP3G02Qs9btQEN5nQB23RMmWYzwM1Z53OMJX2Joq881iOwvQnie8hU7fpS7HmHYPoSq3iFXbbEkep4hhKsOZLgLLlSKvbDTJklrvtMFWHuRxTie4gUrUYTq8QSa_kRXuw6SFai8T8L5HI_2LG9i5jxPctYJT4XJL4Lmea0go8c9Q2a8wAY5n8MmPE-ixkyf4jVYes3iFGeKoQQ3vdP6QHPG2ezzSYzv4uj_AoWJHzJ1iPyABjxPkwkckqjsX3W5PJ-Stbju8misL6W5HB-i6SxytgwvdcvSNXfK7gBTd6n9EDO3HTOJ3-ZJb8L2CZ_jFhw_pexCmN8CZbviGDvOwef7jwKYq87R2D6Ut-4hZMgLgbU4PlSXmy5BV21wdomtMGOF2PweYYW47D8Shbk8b4MWaf0gI7bZ_E9jlekMIlV7jwVYrrH0-x4hR42Q1zpAY-odU7nNIDOW_UC0Sl3RFHrA9IeNk9c6PWB2iczzBmyQIyac8FNmyf0ABjyCxghbfpDkCDtO0dT4e66iJSia7gI0h6rN4OQHOg0AMwYpbqG01ypeYZSW6h4hdIdqbdE22SxPYbTZC15xl87lLK_mLEOpv9ddtPuyKE6hyJvSyVzkKpG01ypNb7LXCi1gcsXqHG-Cpbi7DiFDlrrtMFN4q6-3DDMJjMLpftLnurEnrRFEW3CDmoHm7oLqL0JFzOH2LOP5DD9DyG2jSI2yKG81W_EEmuGnDdQITSOYLVIorsHom9Fo_D-0ic1EKl1zuH7FiR4UeL3E2czkKS5yygC2KoAlWgC1XDHXTnWK8pe74KdKgfd-FZshN89DiGyRdZovlt5i-Y2VGfCWyh71myKne7EIfVGYD3HE6ApdcaP3Gj8F_ZQq4ae6DSGE17q9ACMlqxGojsW9JFapzMGm6TxfUmVoS02QxOc6XVLJUDOW2SxQcsXo4GPHCVyAovYZEDeZ7REkNzrNoKM1iKugFmyTSjyPpAcqLTAzNklMXqHEyS-23SOKcfRHa87R5Pfa3SBDZbjdD1J1mLu-0gTX2w3Q9Dl8j6H1KTxvYbTo_E9CJSgrIMMWOVuuwvnRJ-6g9EiA',
-    'JVqc1PkrbpPF9zyxI5ICZ4y-BFe4Kov1WtA_ZJbI7R9iyCmVCG2SxAcsXpDSPqcVgKXXCS5go8j6LIPsWr4tpBc8bqDF9zpfkcMGbuBPvCFGeKrPAURpm80Ug_JZxSpPgbH6aMv5HlCCp9kcVHmr7iJHebzhE0WtH0yU5gs9gOhaf7H0Wcf0SZzB8zabCS5gkrfpLFGDtRtPhrkdUYi76xxPs-ggg-hNhLzzJli99i1hxSldviRWu_NXh7joHVGFviFUjMD2K2PGKI6_9CRZvvIih-tQhbXaDD5jldj9L2GoF4btWb7jFUWO_F-NsuQUPH3KDjdcjtESYKfzOF2Pv-codbneEFN4qtosjfFWxTNYiroMZIm76yBYiK3fD2LHOaIHep_RAUWuIIXoXI_TBDVajLwypQQ5mMjtH0-_MpHGJVV6rO8URna67TFik7zhE0VqnN8ENmjML1-W-FuS9CVblMwDZJn7Mpb8NGyk1ws8bM4AY5fIKozxV4u88yZblPhbj_EmW5P1WJHyU7UXS4HmHE5-30R4nc8BJlibwPIkiMEiiL8ggbIWRnbcDj5w1Q0_eKkPdKjhQ3yv4xd9s-wjV43D9iuQ9VaLviNavyCD6B5WueoagLPjFXjbFEd62wAyZIm7_i9omsrvIWSVxfkpToDD9SlOgMM3qR6DqNodkQN43QI0d5zOADGXx_pck8owlPUoW5LG-CpgkfNYuelNf7YbUYW36kyy5BZ6rA50qeBGqw1wpdg5ntUKbKXbDXOpDkN1qAtuntD1J1l-sPMYSnyxEkKnDT912z943kR0reMZUYrwVbYXe6sPQ3ziRHyx5hdPgbMYea8RRKYLQnbXBzlwqd1ActM0ZZX6W7_wJFW74BJEaZveAzVnnf5jxfsvYJL0Jlq8HVS4HEyEuu8ih-keTn-27x9Rh73tH1OMxf4vZsf9MWGRwvktk8wtkcgtZsgtk8oCY8UrXYK05gs9gKXXCT6j2hJFdq7gFE2x5Uuv4RpNfd4PQHbYDkB4ruESeLHqT7XuHlSM7yeNvyRXi8T4KFuOvvEpYpnSCGrOB2iY-luAsuQJO36v4RVDc6faDkV6rOMYSX-v5ho_cbTZCz2g0jNr0AVmmsosXY_zVIjuH4G5HFC2F01-tOpPhr8gWIzCJ4rD81S47h5RguMXSqvhRH2t5EqAsecaS3veQ6fbADJkibv-MGCRxPsza57UCzBipcr8LmCQwvUiUoWy5Bhsnc_0J2ibzfIlZpfO_DVqn_keUIKn2RxBc6UbTsY3nQF54kev4hhOg-pNr-UZgPRooAVy0zhdj8HmGFuQwfkeUJO46hxNfaLUBitdoMX3KY7USKDvIVXGKluxHnG1IYnrQpgJW673cb0SeuQtl-EvkddDds8WYKHrZJXMFWrZTqMOdaf2TLfsULoUSq_hFYDXK1uP9T1u0zd63USU6zWa_ECaE23FLWKy4lGD3DSc4zVnnABWwTmsD3iwI23nHJLkXo_HG4T3YasAVqn_VMQYfeo8tBdqqyNx2z5zwSuE_Emd6h1rr_wvVIa43Q9Sd6nbKJcReuZSs9gKUIWz4wg6apLpUsAkkwp9otQEUqbL_S1ejrzsEUSGq90NZM07caXK_T9klsY-dKjR9ihYmQl55UqhBmizHJC15y1ilcz6LWOIuuoSXaX5RpK36SxRg7MfiPNYfa_fJovuWcjxFkh4uyOVBHHW-y1zpNUGNGSSwvAgRXen-lvBIpT9IlSazwI5Z5rQ9SdZfrDzGEp8rt4QQ3Cg0wAyZrrrHUJ1tukbQHO05RxKeqraNFmLveIUV8U6phI3bLA',
-    'JVqc1PkrbpPF9zyxI5ICZ4y-BFe4Kov1WtA_ZJbI7R9iyCmVCG2SxAcsXpDSPqcVgKXXCS5go8j6LIPsWr4tpBc8bqDF9zpfkcMGbuBPvCFGeKrPAURpm80Ug_JZxSpPgbH6aMv5HlCCp9kcVHmr7iJHebzhE0WtH0yU5gs9b5TGCS5gkvgsY5b6LmWYyPkskMX9YMUqYZnQAzWa0wo-ogY6mwEzmNA0ZJXF-i5im_4xaZ3TCECjBWuc0QE2m8__ZMgtYpK36RtAcrXaDD6F9GPKNpvA8iJr2Txqj8HxGVqn6xQ5a67vPYTQFTpsnMQFUpa77TBVh7cJas4zohA1Z5fpQWaYyP01ZYq87D-kFn_kV3yu3iKL_WLFOWyw4RI3aZkPguEWdaXK_CycD26jAjJXiczxI1OXyg4_cJm-8CJHebzhE0WpDDxz1Thv0QI4cangQXbYD3PZEUmBtOgZSavdQHSlB2nONGiZ0AM4cdU4bM4DOHDSNW7PMJL0KF7D-StbvCFVeqzeAzV4nc8BZZ7_ZZz9Xo_zI1O56xtNsuocVYbsUYW-IFmMwPRakMkANGqg0wht0jNomwA3nP1gxfszlsf3XZDA8lW48SRXuN0PQWaY2wxFd6fM_kFyotYGK12g0gYrXaAUhvtghbf6buBVut8RVHmr3Q50pNc5cKcNcdIFOG-j1Qc9btA1lsYqXJP4LmKUxymPwfNXietRhr0jiOpNgrUWe7LnSYK46lCG6yBShehLe63SBDZbjdD1J1mO7x-E6hxSuBxVuyFRisD2LmfNMpP0WIjsIFm_IVmOw_QsXpD1VozuIYPoH1O05BZNhrodT7ARQnLXOJzNATKYve8hRni74BJEettAotgMPW_RAzeZ-jGV-Slhl8z_ZMb7K1yTzPwuZJrK_DBpotsMQ6TaDj5un9YKcKkKbqUKQ6UKcKffQKIIOl-Rw-gaXYK05huAt-8iU4u98SqOwiiMvvcqWrvsHVO16x1Vi77vVY7HLJLL-zFpzARqnAE0aKHVBThrm84GP3av5Uer5EV11zhdj8HmGFuMvvIgUIS36yJXicD1JlyMw_ccTpG26Bp9rxBIreJDd6cJOmzQMWXL_F6W-S2T9CpbkccsY5z9NWmfBGeg0DGVy_suX8D0J4i-IVqKwSddjsT3KFi7IIS43Q9BZpjbDT1uodgQSHux6A0_gqfZCz1tn9L_L2KPwfVJeqzRBEV4rNEERXWu3AxDdtD1J1l-sPMYSnzrWsk3oA112Al_40246iOUCj-k1k-F617BIoit3xE2aKvfF090pukOQHKj0_gqXIGz9htNf-RRir8hZa_4XMc_tABEicEjVKoec6QQiO1azQBWqg5VpiCQCFfBNGzBCVe4HYjBN4zTOrEGN5_gQa_gTK0WfsgUfuZbwBNkibsBV8MNP5TZSav6P7brQa0De8cebKD3TqAWZs8ZXrDhNqMGSb0AWqH7NIX-M3_XGoLaJpP7TKXoHYnfSb0OaKvsZLIcf7QCbMU9ivRFeMZAiQInWYuw4iVKfK77auRNuSWGq90jWIa22w09Zbwlk_dm3VB1p9cleZ7QADFhj7_kF1l-sOA3oA5EeJ3QEjdpmRFHe6TJ-yts3Ey4HXTZO4bvY4i6ADVon80ANluNveUweMwZZYq8_yRWhvJbxitQgrL5XsEsm8TpG0uO9mjXRKnOAEZ3qNkHN2WVw_MYSnrNLpT1Z9D1J22i1Qw6baPI-ixRg8brHU-BseMWQ3Om0wU5jb7wFUiJvPAVSIm58R9Pf68JLmCSt-ksmg975wxBhQ',
+blackbox_tokens = [ #ch, chi, ffi
+    'JVqc1fosb5TG-D2yJJMDaI2_BVi5K4z2W9FAZZfJ7iBjySqWCW6TxQgtX5HTP6gWgabYCi9hpMn7LYTtW78upRg9b6HG-DtgksQHb-FQvSJHeavQAkVqnM4VhPNaxitQgrL7acz6H1GDqNodVXqs7yBSd6nsEUN13U98xBY7bbAYiq_hJIn3JHnM8SNmyzlekMLnGVyBs-VLf7bpTYG46xtMf-MYULMYfbTsI1aI7SZdkfVZje5Uhusjh7foGE2Bte5RhLzwJluT9li-7yRUie4iUrcbgLXlCjxuk8UILV-R2Ee2HYnuE0V1viyPveIURGyt-j5njL4BQpDXI2iNv-8XWKXpDkCDqNoKXL0hhvVjiLrqPJS56xtQiLjdDz-S92nSN6rPATF13lC1GIy_AzRlirzsYtU0acj4HU9_72LB9lWFqtwfRHam6h1hksPsEUN1mswPNGaY_WCXzjRpzwAwZJTFKIvB8SOG6x-EuR6B5kp9s-wcULYafOFHe7HpHoG2GE2vEUGl3j-i2DyiAzRlnQJjyPpflrvtH0R2ud4QQqbfQKbdPp_QNGSU-ixcjvMrXZbHLZLG_2GazQE1m9EKQXWr4RRJrhN0qdxBeN0-oQY8dNcIOJ7RATOW-TJlmPkeUIKn2RxNhrjoDT-Cs-MXR2ye4RNHbJ7hBjhqmwExZMb9NJr-X5LF_DBilMr7XcIjU7fpIIW77yFUthxOgOQWeN4TSrAVd9oPQqMIP3TWD0V33RN4rd8SddgIOl-Rw-gaXYK05ht8rBF3qd9FqeJIrt4XTYO79Fq_IIHlFXmt5kyu5htQgbnrHYLjGXuuEHWs4EFxo9oTR6rcPZ7P_2TFKVqOvyVKfK7TBUhtn9E1mtI0ZcgtXb4iWIzCKIvuI1m67yOFuu5RhbfuUrTlSq4SRqrfQXLUC0R3qt9BcqsPQHWo2Als0DOYyf5iw_YnTH6w1QdKb6HTCG2k3A9AeKreF3uvFXmr5BdHqNkKQKLYCkJ4q9xCe7QZf7joHla58VeJ7iFVjsLyJViIu_MsY5zSNJjRMmLEJUp8rtMFSHmr3w09caTYD0R2reITSXmw5Ak7fqPVB2qc_TWazzBklPYnWb0eUrjpS4PmGoDhF0h-tBlQieoiVozxVI29HoK46BtMreEUdasOR3euFEp7seQVRagNcaXK_C5Thcj5LWCZyf8vYZrQ9Sdqj8HzJVWHuucXUH2v3zNknMH0NWqgxfg5aaHPAThqxOkbTXKk5ww-cKgSivho4FrO_23dRbjsUr0podlQyi2l3Ea17hNFd5zOEUJ6sNUHSm-h0wQ0WYu94hRXfK7gOmy5KXnyPKn8LmKT4Dap2yhs3DJ_sgxClgROmP1SqPxFieJKpNUnmvEkjAVWicsbfcMJc8s4abATjMUshclAsP5UxPhdtAI3nPE2oAJFisEKhPRBph9pwRhLnQxe1yF3wAdRthmE2RJnvfYmidEnnfNhpwpVwjZot-c0pPxTpMn7PaPpK1u1-Dmx_2nUBVK8CTuI3DVptvphk7jqHEFzttsNP4z7dd5Kthc8brTpF0dsns72TbYkiPdu4QY4aLYKL2GRwvIgUHWo6g9Bccgxn9UJLmGjyPoqotgMNVqMvP1t3UmuBWrMF4D0GUuRxvkwXpHH7B5OdsEJXar2G02QtecXg-xXvOETQ4rvUr0sVXqs3B-H-WjVOl-R1wg5b53N-ytZia7gEGPEKov9Zou9AzhrotADOV6QwucZXIGz5RdHeazZCUJvodElVo6z5idckrfqK1uTwfEhUavQAjRZi848sR2JruMn',
+    'JVqc1fosb5TG-D2yJJMDaI2_BVi5K4z2W9FAZZfJ7iBjySqWCW6TxQgtX5HTP6gWgabYCi9hpMn7LYTtW78upRg9b6HG-DtgksQHb-FQvSJHeavQAkVqnM4VhPNaxitQgrL7acz6H1GDqNodVXqs7yBSd6nsEUN13U98xBY7bZ_E9jlekMIoXJPGKl6VyPgpXMD1LZD1WpHJADNlygM6btI2assxY8gAZJTF9SpekssuYZnNAzhw0zWbzAExZsv_L5T4XZLC5xlLcKLlCjxutSST-mbL8CJSmwlsmr_xIUmK1xtEaZveH220AEVqnMz0NYLG6x1ghbfnOZr-Y9JAZZfHGXGWyPgtZZW67Bxv1EavFIes3g5Suy2S9Wmc4BFCZ5nJP7IRRqXV-ixczD-e0zJih7n8IVODx_o-b6DJ7iBSd6nsEUN12j10qxFGrN0NQXGiBWiezgBjyPxhlvtewydakMn5LZP3Wb4kWI7G-16T9SqM7h6Cuxx_tRl_4BFCet9Apdc8c5jK_CFTlrvtH4O8HYO6G3ytEUFx1wk5a9AIOnOkCm-j3D53qt4SeK7nHlKIvvEmi_BRhrkeVbobfuMZUbTlFXuu3hBz1g9Cddb7LV-EtvkqY5XF6hxfkMD0JEl7vvAkSXu-4xVHeN4OQaPaEXfbPG-i2Q0_cafYOp8AMJTG_WKYzP4xk_krXcHzVbvwJ43yVLfsH4DlHFGz7CJUuvBVirzvUrXlFzxuoMX3Ol-Rw_hZie5Uhrwihr8li7v0KmCY0Tec_V7C8laKwymLw_gtXpbI-l_A9liL7VKJvR5OgLfwJIe5Gnus3EGiBjdrnAInWYuw4iVKfK4Sd68RQqUKOpv_NWmfBWjLADaXzABil8suYpTLL5HCJ4vvI4e8Hk-x6CFUh7weT4jsHVKFteZJrRB1pts_oNMEKVuNsuQnTH6w5UqBuewdVYe79FiM8laIwfQkhbbnHX-15x9ViLkfWJH2XJXF-zOWzjRmy_4ya5_PAjVlmNAJQHmvEXWuDz-hAidZi7DiJVaIvOoaToG17CFTir_wJlaNweYYW4Cy5Ed52hJ3rA1BcdMENpr7L5XGKGDD912-9CVbkfYtZsf_M2nOMWqa-1-Vxfgpir7xUojrJFSL8SdYjsHyIoXqToKn2QswYqXWCj12ptwMPnet0gRHbJ7QAjJkl8T0LVqMvBBBeJ3QEUV-o9YXSXyq3RVIosf5K1CCxeocTrcaj8Y3acs8qt1AqhB15FrG9mbOOKcUgvlc0vcpW4Cy9SdYia7gI0h6rN0NMmSWu-0wVYe5DYfYQaX7SZLnF1CdAjRlvBVZyDN9xCeR5DtwtAdPsiV76ClOgMYnoeNTrBWEqdsdcN0ya8AFWtMcTbn6TpPELpEKO68FSI4BZrsiiuMVhfxHsQJ4zv5AsgJVyEKV2yxewQhz60Cp2UOl6EKS4CV7zhhKsvVBlgkuYKYKUavvPpTlXa8FWpHiULz9VJfYUJ4Ic6TxW6ghbsIblOMncOgNP3GWyAswYpThUMoznwtskcMJPmycwfMjS6ILed1MwzZbjb0LX4S25hdHdaXK_T9klsYdhvQqXoO2-B1Pf_ctYYqv4RFSwjKeA1q_IWzVSW6g5htOhbPmHEFzo8sWXrL_S3Ci5Qo8bNhBrBE2aJjfRKcSgarPATF03E69Ko-05ixdjsTyIlCArt4DNWW4GX_gUrvgEliNwPclWI6z5Rc8brHWCDpsnM4BLl6XxPYmeqviBzp7r-gNQIGz5hREdKT-I1WHrN4hjwRw3AE2eg',
+    'JVqc1fosb5TG-D2yJJMDaI2_BVi5K4z2W9FAZZfJ7iBjiLrsHUJ0psv9QGWXyRB12EOy1wk7YJLV-ixetR6M8F_WSW6g0vcpbJHD9TukFnvhUMjtH1F2qOsQQnSZy_0iVJfH7B5hksTpG16DtedMuuc8j7TmKY78IVOFqtwfRHaoDkJ5rBBEe67eD0Km2xN220B3r-YZS7DpIFS4HFCxF0mu5kp6q9sQRHixFEd_s-keVrkbgbLnF0yx5RV63kN4qM3_MVaIy_AiVJsKeeBMsdYIOIHvUoCl1wcvcL0BKk-BxAVTmuYrUIKy2htorNEDRmudzR-A5Em4Jkt9rf84XY-_8SFRdqjYK5ACa9BDaJrKDnfpTrElWJzN_iNVhftuzQJhkbboGIj7Wo_uHkdsntD1J2qPwfMkWIjpHlS371SJ71GC40l5quEUet9CcqQKa6QIbqQJO53UBjZqnP00mP4wYJPFK2KX0AFimP5jlvcvk_UtkcosUYO12gxPdKbYCGrQCGmZzi9mySpik8b_Npj5LmCX-Fq87k-Gu_FWhrzyJ1u_9idYkMcojL_zI4a58CWK7yWK7E6xFU2w6CFTg6jaDDFjptcQQnKXyQw9baHR9ihrndH2KGuQwvQli7vuUIe-JIjpHE-GuuweVIXnTK3dQXOqD0V5q95AptgKbqACaJ3UOp8BZJnMLZLJ_mCZzwFnnQI3aZz_YpLE6RtNcqTnDD5w0wpApdcJbM8FZscAZpv8Mmeg2BFCe7PmSH-2GX2t5Rp7ruQXS68TSa_lHlGJwfMqi-0ig7caULPpGnux4xuAtdoMPmOV2P0vYcctjvIlWInA9SWJwfIlirwfgbgeTrTkG1S17CNWu-0kVoe68yZWvOweVrfqHH2w4BVKe64UR3ziQ6TbP3DSAmSJu-0SRIes3hBIfuBFqgtxowk8baYLPm7QB2vRNpr9M2jLLpDJ-SuMxf02l8n6KpD2WIvvI1mNxShgkPJWhr_xIoPkFHWn4BNFapzO8yVom9D-NWig0wU-c6zfD0h6rNEDRmudzzJkxf1il_gsXL7vIYXmGoCxE0uu4kip3xBGfOEYUbLqHlS5HFWF5kqAsOMUdancPXPWDz923BJDeazdDXDVOW2SxPYbTZDB-ipclMf3L1-Wu-0wVYe56xtNgK3dFkN1pfkqYoe6-zBhhrn6LV2LwvUlf6TWCC1fosf5K6AQiL8kng137SCM-FyO9le67yBRty2VADFq4QY4ao_BBDVmnMHzNluNv_AgRXepzgBDaJrMMHe4KHbNNKQIXa_fKW6zI2_FG4bpMH3zRrQbUJnwZ5j-QY_wOacBabbnF4zkUKkbdN1QwSZsxv1IsgN63iOH3jWNAUqg9z2v_EF14SyDuyKExxVHme07kfVvxDeE2UC5Elmi1yBn2k2WAVaG0wR06zewJGzRFUm-GIW-AFmhCW63Ini-EFOUDFrEL2CtF2SV5CiTClerADFWiLrfEVR5q90qmRN86FS12gxSh7XlCjxslOtUwiaVDH-k1gZUqM3_L2CQvu4TRoit3w9mzz1zp8z_QWaYyEB2qs8CRGmbyz2z2AtMfa3mFERtksT0O6ADbt0CNHqs3A09bZ7O_yRWhsw1pwxy4Vl-sPYnWI-97RJEdpvNEDVnmcv7LWCNvfYjVYXZCkJnmtsQQWaZ2g09a5vL-1V6rN4DNXjmW8czWI3R',
+# this one no longer works?    'JVqc1PkrbpPF9zyxI5ICZ4y-BFe4Kov1WtA_ZJbI7R9iyCmVCG2SxAcsXpDSPqcVgKXXCS5go8j6LIPsWr4tpBc8bqDF9zpfkcMGbuBPvCFGeKrPAURpm80Ug_JZxSpPgbH6aMv5HlCCp9kcVHmr7iJHebzhE0WtH0yU5gs9b5TGCS5gkvgsY5b6LmWYyPkskMX9YMUqYZnQAzWa0wo-ogY6mwEzmNA0ZJXF-i5im_4xaZ3TCECjBWuc0QE2m8__ZMgtYpK36RtAcrXaDD6F9GPKNpvA8iJr2Txqj8HxGVqn6xQ5a67vPYTQFTpsnMQFUpa77TBVh7cJas4zohA1Z5fpQWaYyP01ZYq87D-kFn_kV3yu3iKL_WLFOWyw4RI3aZkPguEWdaXK_CycD26jAjJXiczxI1OXyg4_cJm-8CJHebzhE0WpDDxz1Thv0QI4cangQXbYD3PZEUmBtOgZSavdQHSlB2nONGiZ0AM4cdU4bM4DOHDSNW7PMJL0KF7D-StbvCFVeqzeAzV4nc8BZZ7_ZZz9Xo_zI1O56xtNsuocVYbsUYW-IFmMwPRakMkANGqg0wht0jNomwA3nP1gxfszlsf3XZDA8lW48SRXuN0PQWaY2wxFd6fM_kFyotYGK12g0gYrXaAUhvtghbf6buBVut8RVHmr3Q50pNc5cKcNcdIFOG-j1Qc9btA1lsYqXJP4LmKUxymPwfNXietRhr0jiOpNgrUWe7LnSYK46lCG6yBShehLe63SBDZbjdD1J1mO7x-E6hxSuBxVuyFRisD2LmfNMpP0WIjsIFm_IVmOw_QsXpD1VozuIYPoH1O05BZNhrodT7ARQnLXOJzNATKYve8hRni74BJEettAotgMPW_RAzeZ-jGV-Slhl8z_ZMb7K1yTzPwuZJrK_DBpotsMQ6TaDj5un9YKcKkKbqUKQ6UKcKffQKIIOl-Rw-gaXYK05huAt-8iU4u98SqOwiiMvvcqWrvsHVO16x1Vi77vVY7HLJLL-zFpzARqnAE0aKHVBThrm84GP3av5Uer5EV11zhdj8HmGFuMvvIgUIS36yJXicD1JlyMw_ccTpG26Bp9rxBIreJDd6cJOmzQMWXL_F6W-S2T9CpbkccsY5z9NWmfBGeg0DGVy_suX8D0J4i-IVqKwSddjsT3KFi7IIS43Q9BZpjbDT1uodgQSHux6A0_gqfZCz1tn9L_L2KPwfVJeqzRBEV4rNEERXWu3AxDdtD1J1l-sPMYSnzrWsk3oA112Al_40246iOUCj-k1k-F617BIoit3xE2aKvfF090pukOQHKj0_gqXIGz9htNf-RRir8hZa_4XMc_tABEicEjVKoec6QQiO1azQBWqg5VpiCQCFfBNGzBCVe4HYjBN4zTOrEGN5_gQa_gTK0WfsgUfuZbwBNkibsBV8MNP5TZSav6P7brQa0De8cebKD3TqAWZs8ZXrDhNqMGSb0AWqH7NIX-M3_XGoLaJpP7TKXoHYnfSb0OaKvsZLIcf7QCbMU9ivRFeMZAiQInWYuw4iVKfK77auRNuSWGq90jWIa22w09Zbwlk_dm3VB1p9cleZ7QADFhj7_kF1l-sOA3oA5EeJ3QEjdpmRFHe6TJ-yts3Ey4HXTZO4bvY4i6ADVon80ANluNveUweMwZZYq8_yRWhvJbxitQgrL5XsEsm8TpG0uO9mjXRKnOAEZ3qNkHN2WVw_MYSnrNLpT1Z9D1J22i1Qw6baPI-ixRg8brHU-BseMWQ3Om0wU5jb7wFUiJvPAVSIm58R9Pf68JLmCSt-ksmg975wxBhQ',
 ]
 
 class Session:
     def __init__(self):
         if isWindows:
-            self.logfile = os.getenv('temp') + '\ikabot.log'
+            self.logfile = os.getenv('temp') + '/ikabot.log'
         else:
             self.logfile = '/tmp/ikabot.log'
         self.padre = True
@@ -373,6 +373,8 @@ class Session:
             if r.status_code == 403:
                 print('Failed to log in...')
                 print('Log into the lobby via browser and then press CTRL + SHIFT + J to open up the javascript console')
+                print('If you can not open the console using CTRL + SHIFT + J then press F12 to open Dev Tools')
+                print('In the dev tools there should be a tab called "Console". Press this tab.')
                 print('Paste in the script below and press enter')
                 print("document.cookie.split(';').forEach(x => {if (x.includes('production')) console.log(x)})")
                 
@@ -510,35 +512,64 @@ class Session:
                   }
             resp = self.s.post('https://lobby.ikariam.gameforge.com/api/users/me/loginLink', json=data)
             respJson = json.loads(resp.text)
+            skipGetCookie = False
             if 'url' not in respJson:
                 if retries > 0:
                     return self.__login(retries-1)
-                else:
+                else:                               # 403 is for bad user/pass and 400 is bad blackbox token?
                     msg = 'Login Error: ' + str(resp.status_code) + ' ' + str(resp.reason) + ' ' + str(resp.text)
                     self.writeLog(msg, level = logLevels.ERROR)
                     if self.padre:
                         print(msg)
-                        sys.exit()
+                        print('Failed to log in... Do you want to provide the cookie manually? (Y|N): ')
+                        choice = read(values=['y','Y','n','N'], empty=False)
+                        if choice in ['n','N']:
+                            sys.exit(msg)
+                        while True:
+                            print('Log into the account via browser and then press F12 to open up the dev tools')
+                            print('In the dev tools click the tab "Application" if on Chrome or "Storage" if on Firefox')
+                            print('Within this window, there should be a dropdown menu called "Cookies" on the far left.')
+                            print('Find the "ikariam" cookie and paste it below:')
+                            
+                            ikariam_cookie = read(msg='\nEnter ikariam cookie manually: ').split('=')[-1]
+                            cookie_obj = requests.cookies.create_cookie(domain=self.host, name='ikariam', value=ikariam_cookie)
+                            self.s.cookies.set_cookie(cookie_obj)
+                            try:
+                                # make a request to check the connection
+                                html = self.s.get(self.urlBase, verify=config.do_ssl_verify).text
+                            except Exception:
+                                self.__proxy_error()
+                            skipGetCookie = cookies_are_valid = self.__isExpired(html) is False
+                            if not cookies_are_valid:
+                                print('This cookie is expired. Do you want to try again? (Y|N): ')
+                                choice = read(values=['y','Y','n','N'], empty=False)
+                                if choice in ['n','N']:
+                                    sys.exit(msg)
+                                continue
+                            # TODO check if account is actually the one associated with this email / pass
+                            break
                     else:
+                        self.writeLog('I wanted to ask user for ikariam cookie but he wasn\'t looking', level = logLevels.ERROR)
                         sys.exit(msg)
 
-            url = respJson["url"]
-            match = re.search(r'https://s\d+-\w{2}\.ikariam\.gameforge\.com/index\.php\?', url)
-            if match is None:
-                sys.exit('Error')
+            if not skipGetCookie:
+                url = respJson["url"]
+                match = re.search(r'https://s\d+-\w{2}\.ikariam\.gameforge\.com/index\.php\?', url)
+                if match is None:
+                    sys.exit('Error')
 
-            # set the headers
-            self.s.headers.clear()
-            self.s.headers.update(self.headers)
+                # set the headers
+                self.s.headers.clear()
+                self.s.headers.update(self.headers)
 
-            # set the proxy
-            self.__update_proxy(sessionData=sessionData)
+                # set the proxy
+                self.__update_proxy(sessionData=sessionData)
 
-            # use the new cookies instead, invalidate the old ones
-            try:
-                html = self.s.get(url, verify=config.do_ssl_verify).text
-            except Exception:
-                self.__proxy_error()
+                # use the new cookies instead, invalidate the old ones
+                try:
+                    html = self.s.get(url, verify=config.do_ssl_verify).text
+                except Exception:
+                    self.__proxy_error()
 
         if self.__isInVacation(html):
             msg = _('The account went into vacation mode')
