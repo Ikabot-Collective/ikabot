ea98d549abfc5d7b7b4a738ec8d2a2dc8ae79f08
Matheus Victor <10299707+Kiskadee-dev@users.noreply.github.com>
2023-12-01 17:09:30 -0300

    Fixes config.has_params wasn't updating (#162)

--- ikabot/command_line.py
+++ ikabot/command_line.py
@@ -207,6 +207,7 @@ def menu(session, checkUpdate=True):
     if selected != 0:
         try:
             event = multiprocessing.Event()  # creates a new event
+            config.has_params = len(config.predetermined_input) > 0
             process = multiprocessing.Process(target=menu_actions[selected], args=(session, event, sys.stdin.fileno(), config.predetermined_input), name=menu_actions[selected].__name__)
             process.start()
             process_list.append({'pid': process.pid, 'action': menu_actions[selected].__name__, 'date': time.time(), 'status': 'started'})
