9e6391f4ab6dcf927d87b095674bc89bae85938c
Petar Toshev <petar.toshev@designtechnologies.com>
2024-01-05 00:35:19 +0200

    Fix release versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="ikabot",
-    version='2024.0104.dev222607',
+    version='2024.0104.dev223519',
     author="Petar Toshev",
     author_email="pecata.toshev+ikabot@gmail.com",
     license='MIT',
