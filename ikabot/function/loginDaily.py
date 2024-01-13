219c5fccbc6a5660ad51a354e863acfd311cb6f6
ikagod <65701886+ikagod@users.noreply.github.com>
2024-01-13 08:32:26 +0100

    Add stay on for 30 mins task and refactoring

--- ikabot/function/loginDaily.py
+++ ikabot/function/loginDaily.py
@@ -107,6 +107,28 @@ def do_it(session, wine_city, wood_city, luxury_city, favour_tasks):
         url = 'action=AvatarAction&function=giveDailyActivityBonus&dailyActivityBonusCitySelect={0}&startPageShown=1&detectedDevice=1&autoLogin=on&cityId={0}&activeTab=multiTab2&backgroundView=city&currentCityId={0}&actionRequest={1}&ajax=1'.format(wine_city['id'], actionRequest)
         session.post(url)
 
+        def get_remaining_time_cinetheatre(features):
+            ewt = 999999999
+            if features[0] and 'js_nextPossibleResource' in features[0]:
+                remaining_time_str = re.search(r'js_nextPossibleResource\\">([\S\s]*?)<', features[0]) 
+                assert remaining_time_str, 'Could not get remaining time for wood cultural feature'
+                remaining_time_str = remaining_time_str.group(1).strip()
+                sec_to_wait = timeStringToSec(remaining_time_str) + 60 #add 60s because of rounding errors
+                ewt = sec_to_wait if sec_to_wait < ewt else ewt
+            if features[1] and 'js_nextPossibleTradegood' in features[1]:
+                remaining_time_str = re.search(r'js_nextPossibleTradegood\\">([\S\s]*?)<', features[1])
+                assert remaining_time_str, 'Could not get remaining time for luxury good cultural feature'
+                remaining_time_str = remaining_time_str.group(1).strip()
+                sec_to_wait = timeStringToSec(remaining_time_str) + 60 #add 60s because of rounding errors
+                ewt = sec_to_wait if sec_to_wait < ewt else ewt
+            if features[2] and 'js_nextPossibleFavour' in features[2]:
+                remaining_time_str = re.search(r'js_nextPossibleFavour\\">([\S\s]*?)<', features[2])
+                assert remaining_time_str, 'Could not get remaining time for favour cultural feature'
+                remaining_time_str = remaining_time_str.group(1).strip()
+                sec_to_wait = timeStringToSec(remaining_time_str) + 60 #add 60s because of rounding errors
+                ewt = sec_to_wait if sec_to_wait < ewt else ewt
+            return ewt
+
 
         #collect wood cultural bonus
         if wood_city:
@@ -115,11 +137,7 @@ def do_it(session, wine_city, wood_city, luxury_city, favour_tasks):
             features = html.split('id=\\"VideoRewards\\"')[1].split('ul>')[0].split('li>')
             features = [f for f in features if 'form' in f or 'js_nextPossible' in f]
             if 'js_nextPossibleResource' in features[0]:
-                remaining_time_str = re.search(r'js_nextPossibleResource\\">([\S\s]*?)<', features[0]) 
-                assert remaining_time_str, 'Could not get remaining time for wood cultural feature'
-                remaining_time_str = remaining_time_str.group(1).strip()
-                sec_to_wait = timeStringToSec(remaining_time_str) + 60 #add 60s because of rounding errors
-                earliest_wakeup_time = sec_to_wait if sec_to_wait < earliest_wakeup_time else earliest_wakeup_time 
+                earliest_wakeup_time = get_remaining_time_cinetheatre(features) if get_remaining_time_cinetheatre(features) < earliest_wakeup_time else earliest_wakeup_time 
             else:
                 videoId = re.search(r'name=\\"videoId\\"\s*value=\\"(\d+)\\"', features[0])
                 assert videoId, 'Could not find a match for videoId in html'
@@ -138,11 +156,7 @@ def do_it(session, wine_city, wood_city, luxury_city, favour_tasks):
             features = html.split('id=\\"VideoRewards\\"')[1].split('ul>')[0].split('li>')
             features = [f for f in features if 'form' in f or 'js_nextPossible' in f]
             if 'js_nextPossibleTradegood' in features[1]: 
-                remaining_time_str = re.search(r'js_nextPossibleTradegood\\">([\S\s]*?)<', features[1])
-                assert remaining_time_str, 'Could not get remaining time for luxury good cultural feature'
-                remaining_time_str = remaining_time_str.group(1).strip()
-                sec_to_wait = timeStringToSec(remaining_time_str) + 60 #add 60s because of rounding errors
-                earliest_wakeup_time = sec_to_wait if sec_to_wait < earliest_wakeup_time else earliest_wakeup_time 
+                earliest_wakeup_time = get_remaining_time_cinetheatre(features) if get_remaining_time_cinetheatre(features) < earliest_wakeup_time else earliest_wakeup_time  
             else:
                 videoId = re.search(r'name=\\"videoId\\"\s*value=\\"(\d+)\\"', features[1])
                 assert videoId, 'Could not find a match for videoId in html'
@@ -165,11 +179,7 @@ def do_it(session, wine_city, wood_city, luxury_city, favour_tasks):
             features = html.split('id=\\"VideoRewards\\"')[1].split('ul>')[0].split('li>')
             features = [f for f in features if 'form' in f or 'js_nextPossible' in f]
             if 'js_nextPossibleFavour' in features[2]: 
-                remaining_time_str = re.search(r'js_nextPossibleFavour\\">([\S\s]*?)<', features[2])
-                assert remaining_time_str, 'Could not get remaining time for favour cultural feature'
-                remaining_time_str = remaining_time_str.group(1).strip()
-                sec_to_wait = timeStringToSec(remaining_time_str) + 60 #add 60s because of rounding errors
-                earliest_wakeup_time = sec_to_wait if sec_to_wait < earliest_wakeup_time else earliest_wakeup_time 
+                earliest_wakeup_time = get_remaining_time_cinetheatre(features) if get_remaining_time_cinetheatre(features) < earliest_wakeup_time else earliest_wakeup_time  
             else:
                 videoId = re.search(r'name=\\"videoId\\"\s*value=\\"(\d+)\\"', features[2])
                 assert videoId, 'Could not find a match for videoId in html'
@@ -177,7 +187,20 @@ def do_it(session, wine_city, wood_city, luxury_city, favour_tasks):
                 session.post(f"view=noViewChange&action=AdVideoRewardAction&function=requestBonus&bonusId=53&videoId={str(videoId)}&backgroundView=city&currentCityId={favour_cinetheater_city['id']}&templateView=cinema&actionRequest={actionRequest}&ajax=1")
                 session.setStatus('Waiting 55s to watch video for favour cultural bonus')
                 wait(55)
-                session.post(f"view=noViewChange&action=AdVideoRewardAction&function=watchVideo&videoId={str(videoId)}&backgroundView=city&currentCityId={favour_cinetheater_city['id']}&templateView=cinema&actionRequest={actionRequest}&ajax=1")                
+                session.post(f"view=noViewChange&action=AdVideoRewardAction&function=watchVideo&videoId={str(videoId)}&backgroundView=city&currentCityId={favour_cinetheater_city['id']}&templateView=cinema&actionRequest={actionRequest}&ajax=1")
+
+        #get waiting times for all three cinetheatre features.
+
+        wait(1)    
+
+        if wood_city or luxury_city or favour_cinetheater_city:
+            html = session.post(city_url + str(wine_city['id']))
+            html = session.post(f"view=cinema&visit=1&currentCityId={wine_city['id']}&backgroundView=city&actionRequest={actionRequest}&ajax=1")
+            features = html.split('id=\\"VideoRewards\\"')[1].split('ul>')[0].split('li>')
+            features = [f for f in features if 'form' in f or 'js_nextPossible' in f]
+            earliest_wakeup_time = get_remaining_time_cinetheatre(features) if get_remaining_time_cinetheatre(features) < earliest_wakeup_time else earliest_wakeup_time
+
+        wait(1)
 
         #do favour tasks in wine city
         for task in favour_tasks:
@@ -187,10 +210,10 @@ def do_it(session, wine_city, wood_city, luxury_city, favour_tasks):
             match = re.search(r'currentFavor([\S\s]*?)(\d+)\s*<', html)
             assert match, 'Can not obtain current favour amount'
             if match.group(2) == '2500':
-                #send notification we are full on favour
-                # if not message_sent: #this is commented out because if the user hasn't set the telegram token it will hang the entire module
-                #     sendToBot(session, 'Favour was not collected as you are full on it')
-                #     message_sent = True #we don't want to spam the message, only once is enough
+                # send notification we are full on favour
+                if not message_sent: 
+                    sendToBot(session, 'Favour was not collected as you are full on it')
+                    message_sent = True #we don't want to spam the message, only once is enough
                 break
             rows = html.split('table01')[1].split('table')[0].split('tr')
             rows = [rows[3], rows[5], rows[9], rows[11], rows[15], rows[17], rows[21], rows[23]]
@@ -198,7 +221,7 @@ def do_it(session, wine_city, wood_city, luxury_city, favour_tasks):
             match = re.search(r'"dailyTasksCountdown":{"countdown":{"enddate":(\d+),"currentdate":(\d+),"timeout_ajax', html)
             assert match, 'Can not get remaning daily tasks time'
             sec_remaining = ( int(match.group(1)) - int(match.group(2)) ) - 600
-            earliest_wakeup_time = sec_remaining if sec_remaining < earliest_wakeup_time else earliest_wakeup_time 
+            earliest_wakeup_time = sec_remaining if sec_remaining < earliest_wakeup_time else earliest_wakeup_time
             
         #find capital and get ambro bonus from fountain if it's active
         for id in ids:
@@ -208,46 +231,39 @@ def do_it(session, wine_city, wood_city, luxury_city, favour_tasks):
                     url = 'action=AmbrosiaFountainActions&function=collect&backgroundView=city&currentCityId={0}&templateView=ambrosiaFountain&actionRequest={1}&ajax=1'.format(id, actionRequest)
                     session.post(url)
                 break
+            wait(1)
 
         session.setStatus(f'Last activity @{getDateTime()}, next activity @{getDateTime(time.time()+earliest_wakeup_time)}')
         wait(abs(earliest_wakeup_time), 20) # funnily enough, it's possible to have the earliest wakeup time be a negative number, this happens if there is less than 10 minutes remaning until tasks reset
 
-
-
+def is_collectable(row):
+    progress_left = re.search(r'smallright progress details([\S\s]*?)>([\S\s]*?)<', row).group(2).strip().replace(',','')
+    progress_right = re.search(r'left small progress details([\S\s]*?)>([\S\s]*?)<', row).group(2).strip().replace(',','')
+    return 'textLineThrough' not in row and progress_left == progress_right
+        
 def collect_resource_favour(session, table):
     #literally just collect the first two tasks if they're done
     global wine_city
     passive1 = table[0]
     passive2 = table[1]
-    if 'textLineThrough' not in passive1:
-        progress_left = re.search(r'smallright progress details([\S\s]*?)>([\S\s]*?)<', passive1).group(2).strip().replace(',','')
-        progress_right = re.search(r'left small progress details([\S\s]*?)>([\S\s]*?)<', passive1).group(2).strip().replace(',','')
-        if progress_left == progress_right:
-            taskId=re.search(r'taskId=([\S\s]*?)\\"', passive1).group(1)
-            session.post(f"action=CollectDailyTasksFavor&taskId={taskId}&ajax=1&backgroundView=city&currentCityId={wine_city['id']}&templateView=dailyTasks&actionRequest={actionRequest}&ajax=1")
-            wait(1)
-    if 'textLineThrough' not in passive2:
-        progress_left = re.search(r'smallright progress details([\S\s]*?)>([\S\s]*?)<', passive2).group(2).strip().replace(',','')
-        progress_right = re.search(r'left small progress details([\S\s]*?)>([\S\s]*?)<', passive2).group(2).strip().replace(',','')
-        if progress_left == progress_right:
-            taskId=re.search(r'taskId=([\S\s]*?)\\"', passive2).group(1)
-            session.post(f"action=CollectDailyTasksFavor&taskId={taskId}&ajax=1&backgroundView=city&currentCityId={wine_city['id']}&templateView=dailyTasks&actionRequest={actionRequest}&ajax=1")
-            wait(1)
+    
+    if is_collectable(passive1):
+        taskId=re.search(r'taskId=([\S\s]*?)\\"', passive1).group(1)
+        session.post(f"action=CollectDailyTasksFavor&taskId={taskId}&ajax=1&backgroundView=city&currentCityId={wine_city['id']}&templateView=dailyTasks&actionRequest={actionRequest}&ajax=1")
+        wait(1)
+    
+    if is_collectable(passive2):
+        taskId=re.search(r'taskId=([\S\s]*?)\\"', passive2).group(1)
+        session.post(f"action=CollectDailyTasksFavor&taskId={taskId}&ajax=1&backgroundView=city&currentCityId={wine_city['id']}&templateView=dailyTasks&actionRequest={actionRequest}&ajax=1")
+        wait(1)
 
-
-
-    pass
 def look(session, table):
     #send request to look at all three (can't be bothered to check which one has to be done exactly), collect bonus
     global earliest_wakeup_time, wine_city
     for r in table:
         if 'task_amount_28' in r: #visit shop
-            if 'textLineThrough' not in r:
-                progress_left = re.search(r'smallright progress details([\S\s]*?)>([\S\s]*?)<', r).group(2).strip().replace(',','')
-                progress_right = re.search(r'left small progress details([\S\s]*?)>([\S\s]*?)<', r).group(2).strip().replace(',','')
-                if progress_left == progress_right:
-                    taskId=re.search(r'taskId=([\S\s]*?)\\"', r).group(1)
-                    session.post(f"action=CollectDailyTasksFavor&taskId={taskId}&ajax=1&backgroundView=city&currentCityId={wine_city['id']}&templateView=dailyTasks&actionRequest={actionRequest}&ajax=1")
+                if is_collectable(r):
+                    session.post(f"action=CollectDailyTasksFavor&taskId=28&ajax=1&backgroundView=city&currentCityId={wine_city['id']}&templateView=dailyTasks&actionRequest={actionRequest}&ajax=1")
                     wait(1)
                 else:
                     session.post(f"view=premium&linkType=2&backgroundView=city&currentCityId={wine_city['id']}&templateView=dailyTasks&actionRequest={actionRequest}&ajax=1")
@@ -256,12 +272,8 @@ def look(session, table):
                     wait(1)
 
         if 'task_amount_27' in r: #open inventory
-            if 'textLineThrough' not in r:
-                progress_left = re.search(r'smallright progress details([\S\s]*?)>([\S\s]*?)<', r).group(2).strip().replace(',','')
-                progress_right = re.search(r'left small progress details([\S\s]*?)>([\S\s]*?)<', r).group(2).strip().replace(',','')
-                if progress_left == progress_right:
-                    taskId=re.search(r'taskId=([\S\s]*?)\\"', r).group(1)
-                    session.post(f"action=CollectDailyTasksFavor&taskId={taskId}&ajax=1&backgroundView=city&currentCityId={wine_city['id']}&templateView=dailyTasks&actionRequest={actionRequest}&ajax=1")
+                if is_collectable(r):
+                    session.post(f"action=CollectDailyTasksFavor&taskId=27&ajax=1&backgroundView=city&currentCityId={wine_city['id']}&templateView=dailyTasks&actionRequest={actionRequest}&ajax=1")
                     wait(1)
                 else:
                     session.post(f"view=inventory&backgroundView=city&currentCityId={wine_city['id']}&templateView=dailyTasks&actionRequest={actionRequest}&ajax=1")
@@ -277,35 +289,33 @@ def capture_runs(session, table):
 def donate_wood(session, table):
     #find city with most wood and donate number of it to lux resource or wood production (randomly chosen), collect bonus
     pass
+def stay_online_30_mins(session, table):
+    global earliest_wakeup_time, wine_city
+    for r in table:
+        if 'task_amount_23' in r: # stay online for 30 mins
+            if is_collectable(r):
+                session.post(f"action=CollectDailyTasksFavor&taskId=23&ajax=1&backgroundView=city&currentCityId={wine_city['id']}&templateView=dailyTasks&actionRequest={actionRequest}&ajax=1")
+                wait(1)
+            else:
+                earliest_wakeup_time = 31 * 60 if 31 * 60 < earliest_wakeup_time else earliest_wakeup_time 
 def complete_tasks(session, table):
     #collect the last two tasks if they're available, this one should always run last
     global wine_city
     passive1 = table[6]
     passive2 = table[7]
-    if 'textLineThrough' not in passive1:
-        progress_left = re.search(r'smallright progress details([\S\s]*?)>([\S\s]*?)<', passive1).group(2).strip().replace(',','')
-        progress_right = re.search(r'left small progress details([\S\s]*?)>([\S\s]*?)<', passive1).group(2).strip().replace(',','')
-        if progress_left == progress_right:
-            taskId=re.search(r'taskId=([\S\s]*?)\\"', passive1).group(1)
-            session.post(f"action=CollectDailyTasksFavor&taskId={taskId}&ajax=1&backgroundView=city&currentCityId={wine_city['id']}&templateView=dailyTasks&actionRequest={actionRequest}&ajax=1")
-            wait(1)
-    if 'textLineThrough' not in passive2:
-        progress_left = re.search(r'smallright progress details([\S\s]*?)>([\S\s]*?)<', passive2).group(2).strip().replace(',','')
-        progress_right = re.search(r'left small progress details([\S\s]*?)>([\S\s]*?)<', passive2).group(2).strip().replace(',','')
-        if progress_left == progress_right:
-            taskId=re.search(r'taskId=([\S\s]*?)\\"', passive2).group(1)
-            session.post(f"action=CollectDailyTasksFavor&taskId={taskId}&ajax=1&backgroundView=city&currentCityId={wine_city['id']}&templateView=dailyTasks&actionRequest={actionRequest}&ajax=1")
-            wait(1)
+    if is_collectable(passive1):
+        session.post(f"action=CollectDailyTasksFavor&taskId=30&ajax=1&backgroundView=city&currentCityId={wine_city['id']}&templateView=dailyTasks&actionRequest={actionRequest}&ajax=1")
+        wait(1)
+    if is_collectable(passive2):
+        session.post(f"action=CollectDailyTasksFavor&taskId=31&ajax=1&backgroundView=city&currentCityId={wine_city['id']}&templateView=dailyTasks&actionRequest={actionRequest}&ajax=1")
+        wait(1)
 
 tasks = {'Collect resource favour': collect_resource_favour,
          'Look at hightscore/shop/inventory': look,
+         'Stay online for 30 mins': stay_online_30_mins,
+
          #'Conduct capture runs': capture_runs, #coming soon
          #'Donate wood': donate_wood,
 
-         'Complete 2/all tasks': complete_tasks,
+         'Complete 2/all tasks': complete_tasks, # must be last
          }
-task_text_to_taks = {'Collect some', 'Collect resource favour', #TODO this won't work because of different languages, find a different way to conjoin table row with task name (task id maybe?)
-                     'Conduct capture runs', 'Conduct capture runs',
-
-
-}
