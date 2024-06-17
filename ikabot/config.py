ca7582575a5b1fc396514aa77ad1f5c76ef85012
johnvittu <joni.makela1337@gmail.com>
2024-06-17 13:17:03 +0300

    Change build date

--- ikabot/config.py
+++ ikabot/config.py
@@ -9,7 +9,7 @@ import random
 
 # Version changed automatically by the release pipeline
 IKABOT_VERSION = "7.0.3"
-BUILD_DATE = "27.04.2024"
+BUILD_DATE = "17.06.2024"
 
 IKABOT_VERSION_TAG = "v" + IKABOT_VERSION + " - " + BUILD_DATE
 
