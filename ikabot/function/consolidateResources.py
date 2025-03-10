20d87e7003ca347bf73837c98358bcc9ebff016f
Tomas <tomis.jasulaitis@gmail.com>
2025-03-10 09:08:03 +0200

    Adding running first run and improving structure

--- ikabot/function/consolidateResources.py
+++ ikabot/function/consolidateResources.py
@@ -137,22 +137,16 @@ def do_it(session, limits, sourceCityId, destinationCityId, intervalInHours):
     intervalInHours : int
     """
 
-    sourceCity = getCity(session.get(city_url + str(sourceCityId)))
-    destinationCity = getCity(session.get(city_url + str(destinationCityId)))
-
-    hasConsolidated = {}
-    routes = []  # Store all routes for batch execution
-    lastResetTime = datetime.datetime.now()
+    firstRun = True
+    nextRunTime = datetime.datetime.now()
 
     while True:
         currentTime = datetime.datetime.now()
-        timeElapsed = (currentTime - lastResetTime).total_seconds()
-        if timeElapsed >= intervalInHours * 60 * 60:  
-            hasConsolidated.clear()
-            lastResetTime = currentTime  # Update the last time reseted the list
-        else:
-            time.sleep(60 * 60)
-
+        
+        if currentTime < nextRunTime and not firstRun:
+            time.sleep(60)
+            continue
+        
         try:
             sourceCity = getCity(session.get(city_url + str(sourceCityId)))
             destinationCity = getCity(session.get(city_url + str(destinationCityId)))
@@ -188,37 +182,28 @@ def do_it(session, limits, sourceCityId, destinationCityId, intervalInHours):
                 sourceCity['name'], 
                 intervalInHours
             ))
-
-            nextExecutionTime = lastResetTime + datetime.timedelta(hours=intervalInHours)
-            session.setStatus(
-                f" {sourceCity['name']} -> {destinationCity['name']} | Next at {getDateTime(nextExecutionTime.timestamp())}"
+        else: 
+            route = (
+                sourceCity,
+                destinationCity,
+                destinationCity["islandId"],
+                toSend.get("wood", 0),
+                toSend.get("wine", 0),
+                toSend.get("marble", 0),
+                toSend.get("crystal", 0),
+                toSend.get("sulfur", 0),
             )
 
-            time.sleep(intervalInHours * 60 * 60)
-            continue
-
-        routes.append((
-            sourceCity,
-            destinationCity,
-            destinationCity["islandId"],
-            toSend.get("wood", 0),
-            toSend.get("wine", 0),
-            toSend.get("marble", 0),
-            toSend.get("crystal", 0),
-            toSend.get("sulfur", 0),
-        ))
-
-        if routes:
-            executeRoutes(session, routes, useFreighters=False)
+            executeRoutes(session, [route], useFreighters=False)
 
             resourceMessage = ", ".join([f"{amt} {res}" for res, amt in toSend.items() if amt > 0])
             sendToBot(session, f"Sent {resourceMessage} from {sourceCity['name']} to {destinationCity['name']}")
 
-            routes.clear()
+        nextExecutionTime = datetime.datetime.now() + datetime.timedelta(hours=intervalInHours)
+        session.setStatus(
+            f" {sourceCity['name']} -> {destinationCity['name']} | Next at {getDateTime(nextExecutionTime.timestamp())}"
+        )
 
-            nextExecutionTime = lastResetTime + datetime.timedelta(hours=intervalInHours)
-            session.setStatus(
-                f" {sourceCity['name']} -> {destinationCity['name']} | Next at {getDateTime(nextExecutionTime.timestamp())}"
-            )
+        firstRun = False
 
         time.sleep(60 * 60)
