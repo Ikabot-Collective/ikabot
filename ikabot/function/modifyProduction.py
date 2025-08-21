f39765e2e142914cccc21cd860238042ba8ba55e
susman <61547860+susikaman@users.noreply.github.com>
2025-08-22 00:24:20 +0300

    Reconstructed code
    - Reconstructed the entire code
    - Ability to use it for multiple cities
    - Modify wood, trade or both
    - Set a percentage of the maximum amount of workers
    - Optional overcharge of workers if selected 100% workers

--- ikabot/function/modifyProduction.py
+++ ikabot/function/modifyProduction.py
@@ -25,83 +25,103 @@ def modifyProduction(session, event, stdin_fd, predetermined_input):
     config.predetermined_input = predetermined_input
     try:
         banner()
-
-        print("In what city do you want modify your production?")
-        city = chooseCity(session)
+        mod_msg = "In which cities do you want to modify production?"
+        city_ids, _ = ignoreCities(session, msg=mod_msg)
         
+        cities_to_process = {}
+        #print("DEBUG: Fetching detailed information for selected cities...")
+        for city_id in city_ids:
+            html = session.get(city_url + city_id)
+            city = getCity(html)
+            if city['islandId'] not in cities_to_process:
+                cities_to_process[city['islandId']] = {'island': getIsland(session.get(island_url + city['islandId'])), 'cities': []}
+            cities_to_process[city['islandId']]['cities'].append(city)
+            #print(f"DEBUG: Got city data for {city_id}")
+        
+
+        print("You want to modify the number of workers in Wood (1), Tradegood (2) or Both (3)?")
+        res_choice = read(min=1, max=3)
+        resource_types_to_modify = []
+        if res_choice == 1:
+            resource_types_to_modify.append("resource")
+        elif res_choice == 2:
+            resource_types_to_modify.append("tradegood")
+        elif res_choice == 3:
+            resource_types_to_modify.extend(["resource", "tradegood"])
+            
+            
+        print("What % of production do you want to use? (Default 100%)")
+        percentageWorkers = read(min=0, max=100, default=100)
+        if percentageWorkers == 100:
+            print("Would you like to use overcharge? (y/N)")
+            overcharge = read(values=["y", "Y", "n", "N", ""])
+            if overcharge.lower() == "y":
+                use_overcharge = True
+            else:
+                use_overcharge = False
+        else:
+            use_overcharge = False
+            
         banner()
-        
-        # print(str(city))
+        # loop through each selected city
+        current_city_id_for_request = getCity(session.get())['id']
+        # Loop through islands first
+        for islandId, island_data in cities_to_process.items():
+            island = island_data['island']
+            resource_name = tradegoods_names[0]
+            tradegood_name = tradegoods_names[int(island["tipo"])]
+            #print(f"DEBUG: Visited island {islandId}")
 
-        islandId = city["islandId"]
-        html = session.get(island_url + islandId)
-        island = getIsland(html)
-        
-        print('==> TEST')
-        print(island)
-        print('==> TEST\n1')
+            # Then loop through cities on that island
+            for city in island_data['cities']:
+                # Change the session context to the current city
+                session.post(params={
+                    'action': 'header', 'function': 'changeCurrentCity',
+                    'actionRequest': actionRequest, 'cityId': city['id'],
+                    'oldView': 'city', 'backgroundView': 'city',
+                    'currentCityId': current_city_id_for_request, 'ajax': '1'
+                })
+                # Update the current city ID for the next request
+                current_city_id_for_request = city['id']
+                #print(f"DEBUG: session.post was made, current city is {city['id']} and island is {islandId}")
 
-        island_type = island["tipo"]
-        resource_name = tradegoods_names[0]
-        tradegood_name = tradegoods_names[int(island_type)]
-
-        # get resource information
-        url = "view=resource&type=resource&islandId={0}&backgroundView=island&currentIslandId={0}&actionRequest={1}&ajax=1".format(
-            islandId, actionRequest
-        )
-        resp = session.post(url)
-        resp = json.loads(resp, strict=False)
-        
-        citizens, workers = re.findall(
-            r'<span class="scroll_view" (.*?)</span>', resp[1][1][1]
-        )
-        
-        citizens = citizens.replace(",", "").replace(".", "").replace('id="valueCitizens">',"")
-        citizens = int(citizens)
-        workers = workers.replace(",", "").replace(".", "").replace('id="valueWorkers">',"")
-        workers = int(workers)
+                # Loop through only the resource types the user chose to modify
+                for resource_type in resource_types_to_modify:
+                    # Fetch data, calculate, and set workers in one sequence
+                    
+                    # Fetch data for the current resource type
+                    url = f"view={resource_type}&type={resource_type}&islandId={islandId}&cityId={city['id']}&backgroundView=island&currentIslandId={islandId}&actionRequest={actionRequest}&ajax=1"
+                    resp = session.post(url)
+                    resp_json = json.loads(resp, strict=False)
+                    
+                    template_data = resp_json[2][1]
+                    slider_data = template_data['js_ResourceSlider']['slider']
+                    max_normal = slider_data['max_value']
+                    total_max = slider_data['max_value'] + slider_data['overcharge']
+                    #print(f"DEBUG: session.post was made, current city is {city['id']}, current island is {islandId}\nresource type is {resource_type}, max_normal is {max_normal} and total_max is {total_max}")
+                    
+                    # Calculate finalWorkers using the data we just fetched
+                    if percentageWorkers == 100:
+                        finalWorkers = total_max if use_overcharge else max_normal
+                    else:
+                        finalWorkers = int(max_normal / 100 * percentageWorkers)
+                    
+                    # Set the workers
+                    session.post(params={
+                        "islandId": islandId, "cityId": city["id"],
+                        "type": resource_type, "screen": resource_type,
+                        "action": "IslandScreen", "function": "workerPlan",
+                        "rw": finalWorkers, "templateView": resource_type,
+                        "actionRequest": actionRequest, "ajax": "1"
+                    })
+                    
+                    selected_good_name = resource_name if resource_type == "resource" else tradegood_name
+                    print(f"{finalWorkers} workers set for {selected_good_name} in {city['name']}.")
                 
-        msg = "You want to modify the number of workers in {} (1) or in {} (2)?:".format(
-                resource_name, tradegood_name
-            )
-        
-        whereToModProd = read(msg=msg, min=1, max=2)
-        whereToModProd = "resource" if whereToModProd == 1 else "tradegood"
-        
-        msg = "What % of all your citizens you want to put in the {} (0: None -> 100: Max; Default 100%)?:".format(
-                resource_name if whereToModProd == "resource" else tradegood_name, citizens+workers
-            )
-        
-        finalWorkers = read(msg=msg, min=0, max=100, default=100)        
-        finalWorkers =  citizens+workers if finalWorkers == 100 else int(finalWorkers/100 * (citizens+workers))
-         
-        print("")
-        
-        # print("To set: {} workers in {}[{}]".format(finalWorkers, islandId, city["id"]))
-        
-        session.post(
-            params={
-                "islandId": islandId,
-                "cityId": city["id"],
-                "type": whereToModProd,
-                "screen": whereToModProd,
-                "action": "IslandScreen",
-                "function": "workerPlan",
-                "rw": int(finalWorkers),
-                "templateView": whereToModProd,
-                "actionRequest": actionRequest,
-                "ajax": "1"
-            }
-        )
-        
-        print("You've set {} worker(s) in the {}.".format(
-            finalWorkers, resource_name if whereToModProd == "resource" else tradegood_name
-        ))
-
+        print("\nAll productions have been set!")
         enter()
         event.set()
         
-        return
     except KeyboardInterrupt:
         event.set()
-        return
+        return 
